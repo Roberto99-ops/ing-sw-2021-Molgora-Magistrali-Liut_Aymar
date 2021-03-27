61ccf35a6c989a8b77e724ed75f1b444d3736c85
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-27 22:38:39 +0100

    classes structures

---
+++ src/main/java/it/polimi/ingsw/Resource.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw;
+
+public class Resource {
+    private enum type {G, B, P, Y, R, W};
+    public enum getType()
+    {
+        return type;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/ResourceStructure.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw;
+import java.util.ArrayList;
+public class ResourceStructure {
+    private ArrayList <Resource> vector;
+
+    public ArrayList<Resource> getVector() {
+        return vector;
+    }
+
+    public void setVector(ArrayList<Resource> vector) {
+        this.vector = vector;
+    }
+
+    public *Resource Order(*Resource)
+    {
+
+    }
+}
