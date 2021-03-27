4fa06b39910bb5f4ac053846bcfecfd0fa07609e
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-27 17:56:25 +0100

    test

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,11 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        TestClass.print(15);
     }
+
 }
---
+++ src/main/java/it/polimi/ingsw/TestClass.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw;
+class TestClass {
+    public static void print(int c) {
+        System.out.println("the number is " + c);
+    }
+}
