507d63a8398d81270f0f6b9965f73a2e1fb06bae
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-27 22:02:33 +0100

    classes structures

---
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw;
+
+public class Game {
+    private int numberofplayers;
+    public static void main( String[] args )
+    {
+
+    }
+    private void Shuffle()
+    {
+
+    }
+    private boolean Endgame()
+    {
+
+    }
+    private int Victory()
+    {
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/Turn.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw;
+
+public class Turn {
+    private Player actualplayer;
+    private int action;
+
+    private Developementcard ShopCard() {
+
+    }
+
+    public *Resource Buyresource(boolean choice, int quantity) {
+
+    }
+
+    private void Production(Cartasviluppo carta) {
+
+    }
+}
