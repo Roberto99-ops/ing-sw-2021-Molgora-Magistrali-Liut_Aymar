306991b28099ec80c435d3be301d2191f676d3c7
fm <francesco.magistrali@mail.polimi.it>
2021-03-29 21:11:29 +0200

    classes structure

---
+++ src/main/java/it/polimi/ingsw/Market.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+public class Market {
+    private ResourceStructure firstrow;
+    private ResourceStructure secondrow;
+    private ResourceStructure thirdrow;
+    private Resource extraball;
+
+    public Resource GetResource (boolean check, int number)
+
+    public void SetResource (boolean check, int number)
+}
