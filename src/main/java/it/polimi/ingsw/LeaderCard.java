3b7f7768d9769db024a0e060733b0e5e640eb2d2
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-30 17:18:19 +0200

    switch started, priceC has to be implemented, implemented an additional method to print cards properties.

--- src/main/java/it/polimi/ingsw/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/LeaderCard.java
@@ -1,19 +1,94 @@
 package it.polimi.ingsw;
-
-import java.lang.reflect.Array;
-
 public class LeaderCard extends Card{
     private ResourceStructure priceR;
-    private DevelopeCard priceC[]; // lungo 2?
+    private DevelopeCard priceC[];
+    private int pv;
     private int skill;
 
-    @Override
+    //@Override
     public LeaderCard getCard(int number)
     {
-        //switch
+        //ArrayList<Resource> array = new ArrayList<Resource>();
+        //Resource r = new Resource();
+        //LeaderCard card = new LeaderCard();
+        this.priceR = new ResourceStructure();
+        this.priceC = new DevelopeCard[2];
+
+        switch(number) {
+            case 0:
+                //r.setColour('Y');
+                //this.priceR.getVector().add('Y');
+                //this.priceR.AddResource('Y');
+                //r.setColour('G');
+                //this.priceR.getVector().add('G');
+                //this.priceR.AddResource('G');
+                this.priceR=null;
+                //this.priceC[0];
+                this.pv=2;
+                this.skill=0;
+                break;
+            case 1:
+                this.priceR=null;
+                this.priceC=null;
+                this.pv=2;
+                this.skill=1;
+                break;
+            case 2:
+                this.priceR=null;
+                this.priceC=null;
+                this.pv=2;
+                this.skill=2;
+                break;
+            case 3:
+                this.priceR=null;
+                this.priceC=null;
+                this.pv=3;
+                this.skill=3;
+                break;
+            case 4:
+                this.priceR.AddResource(5, 'Y');
+                this.priceC=null;
+                this.pv=3;
+                this.skill=4;
+                break;
+            case 5:
+                this.priceR.AddResource(5, 'W');
+                this.priceC=null;
+                this.pv=3;
+                this.skill=5;
+                break;
+            case 6:
+                this.priceR.AddResource(5, 'P');
+                this.priceC=null;
+                this.pv=3;
+                this.skill=6;
+                break;
+            case 7:
+                this.priceR.AddResource(5, 'B');
+                this.priceC=null;
+                this.pv=3;
+                this.skill=7;
+                break;
+            case 8:
+                this.priceR=null;
+                this.priceC=null;
+                this.pv=3;
+                this.skill=8;
+                break;
+        }
+        return this;
     }
     public void Skill(int number)
     {
 
+    }
+    public void Print()
+    {
+        System.out.println("pv: " + this.pv);
+        if (this.priceC != null)  System.out.println("priceC: " + this.priceC);
+        else System.out.println("priceC: null");
+        if (this.priceR != null)  System.out.println("priceR: " + this.priceR.getVector());
+        else System.out.println("priceR: null");
+        System.out.println("skill: " + this.skill + "\n");
     }
 }
