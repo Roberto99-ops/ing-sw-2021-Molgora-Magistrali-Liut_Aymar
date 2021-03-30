2233432e0b1416e17dae25dbaa69dbf829b1e361
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-30 17:21:03 +0200

    type enum deleted, now it is basically a Char, so it's quite useless. we have to decide what to do about that

--- src/main/java/it/polimi/ingsw/Resource.java
+++ src/main/java/it/polimi/ingsw/Resource.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw;
-
 public class Resource {
-    private enum type {G, B, P, Y, R, W};
-    private type colour;
+    //private enum type {G, B, P, Y, R, W}; sta classe non la sto proprio usando
+    private char colour;
 
-    public type getColour() {
+    public void setColour(char colour) {
+        this.colour = colour;
+    }
+    public char getColour() {
         return colour;
     }
 }
