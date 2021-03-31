64920e71b10c308a5bb0d04cd5027311e3e1ed40
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-31 19:22:13 +0200

    Random Algorythm implemented

--- src/main/java/it/polimi/ingsw/Deck.java
+++ src/main/java/it/polimi/ingsw/Deck.java
@@ -1,15 +1,23 @@
 package it.polimi.ingsw;
-
+import java.util.Random;
 import java.util.ArrayList;
 
 public abstract class Deck {
-    private ArrayList<Card> structure;
-    public Card getDeck()
+    private ArrayList<Object> structure;
+    public Object getDeck()
     {
 
     }
-    public void setDeck(ArrayList<Card> structure)
+    public ArrayList<Object> setDeck() //non so se funzionano pe riferimento o cosa
     {
-
+        ArrayList<Object> mixedStructure = new ArrayList<Object>();
+        Random mixer = new Random();
+        while(structure.size() > 0)
+        {
+            int mix = mixer.nextInt(structure.size());
+            mixedStructure.add(structure.get(mix));
+            structure.remove(mix);
+        }
+        return mixedStructure;
     }
 }
