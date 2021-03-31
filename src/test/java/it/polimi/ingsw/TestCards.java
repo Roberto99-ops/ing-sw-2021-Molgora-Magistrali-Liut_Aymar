b2d7c27ef1365866e9b4353fa86e88c11cb8b12e
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-03-31 18:05:21 +0200

    tests leadercard and developecard

--- src/test/java/it/polimi/ingsw/TestCards.java
+++ src/test/java/it/polimi/ingsw/TestCards.java
@@ -3,13 +3,13 @@ package it.polimi.ingsw;
 public class TestCards {
     public static void main( String[] args )
     {
-        /*LeaderCard card = new LeaderCard();
-        for(int i=4; i<8; i++)
+        LeaderCard card = new LeaderCard();
+        for(int i=0; i<16; i++)
         {
             card.getCard(i);
             System.out.println("LeaderCard "+i+" :");
             card.Print();
-        }*/
+        }
         DevelopeCard card1 = new DevelopeCard();
         for(int i=0; i<48; i++)
         {
