78449deed9d98b5be4720b10f6d7779065a5c1a3
juan <juandavid.liut@mail.polimi.it>
2021-04-02 17:08:56 +0200

    method 'Vatican Report': done

--- src/main/java/it/polimi/ingsw/FaithTrack.java
+++ src/main/java/it/polimi/ingsw/FaithTrack.java
@@ -2,9 +2,9 @@ package it.polimi.ingsw;
 
 public class FaithTrack {
     private int position;
-    private int PV; //a seconda di dove si trova il segnalino riceve tot PV: ex cella #17==9PV
-                    //Partirebbe da 0 così mentre avanza "raccoglie" i PV sul tracciato
-                    //oppure si può usare il PV di PLayer?
+    private int PV_Track=0; //a seconda di dove si trova il segnalino riceve tot PV: ex cella #17==9PV
+                            //Partirebbe da 0 così mentre avanza "raccoglie" i PV sul tracciato
+                            //oppure si può usare il PV di PLayer?
 
     //prende la cella del tracciato in cui si trova il segnalino Fede
     public int GetPosition(){
@@ -12,23 +12,43 @@ public class FaithTrack {
     }
 
     //verifica che il segnalino si trovi nell'area VaticanReport
-    public int VaticanReport(int position){
-        int VR=0; //lo inseriamo qui oppure in Player? (spiegazione in Player)
-        /*
-        se lo mettessimo qui
-        if ((ogg position>=8 && ogg position<=15)&& VR==0){
-
-        } else if ((ogg position>=16 && ogg position<=23)&& VR==1){
-
-        } else if ((ogg position==24)&& VR==1){
+    public void VaticanReport(int position){
+        int i;
+        int VR = Player.getVR(); // VR = vatican report
+        // caso in cui un giocatore arrivi per primo a fare il VR
+        if ((position>=8 && position<=15)&& VR==0){
+            PV_Track+=2; //il VR della prima area vale 2
+            //controllo che i giocatori si trovino nell'area del VR e se lo sono sommo i punti
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>4 && Player/*numero*/.trackposition<9){
+                    Player/*numero*/.PV+=2;
+                }
+            }
+            Player.setVR(1); //setta il VR di tutti i giocatori a 1 perchè la prima area è stata superata
+        } else if ((position>=16 && position<=23)&& VR==1){
+            PV_Track+=3; // seconda area vale 3
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>11 && Player/*numero*/.trackposition<17){
+                    Player/*numero*/.PV+=3;
+                }
+            }
+            Player.setVR(2);
+        } else if ((position==24)&& VR==1){
+            PV_Track+=4; // terza area vale 4
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>18) {
+                    Player/*numero*/.PV=+ 4;
+                }
+            }
             //chiama EndGame() e Victory()
+            Game.EndGame();
+            Game.Victory();
         }
-        */
 
     }
 
     public void SetPosition(int position){
-        return position;
+        this.position=position;
     }
 
 }
