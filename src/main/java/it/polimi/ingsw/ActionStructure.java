7388988e781da1e3d16e4fc523dd22074860d58f
juan <juandavid.liut@mail.polimi.it>
2021-04-02 17:03:45 +0200

    method 'PickSignal': done method 'ShuffleSignal': done, but it needs to be discussed (other idea available)

--- src/main/java/it/polimi/ingsw/ActionStructure.java
+++ src/main/java/it/polimi/ingsw/ActionStructure.java
@@ -1,17 +1,25 @@
 package it.polimi.ingsw;
 
 public class ActionStructure {
-    private ActionSignal structure[];
+    private ActionSignal[] structure;
+    private int AS_Counter=0; //ActionSignal_counter conta il segnalino che è stato preso (da 0 a 6 -> posti nella pila)
 
-    //
-    public int PicSignal(){
-
+    //metodo che prende il primo segnalino della pila e lo rimette in fondo
+    public int PickSignal(ActionStructure[] structure, int AS_Counter){
+        ActionSignal.Action(structure[AS_Counter]); //chiamo il metodo del segnalino in base al suo numero
+        AS_Counter++;
+        if (AS_Counter==7){ //se arrivo alla fine della pila torno su, al posto 0
+            AS_Counter=0;
+        }
+        return AS_Counter;
     }
 
     //metodo che mischia le carte del mazzo
     public void ShuffleSignal(){
-        /*oggetto della classe Game*/.shuffle();
-
+        theGame = new Game();
+        theGame.Shuffle();
+        //oppure ho trovato questo metodo per mischiare un array usando libreria standard di Java
+        //Collections.shuffle(Arrays.asList(structure))
     }
 
 }
