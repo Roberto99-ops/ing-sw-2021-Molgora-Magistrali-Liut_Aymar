db0d46fd5823ed5c0128031286df5c2b6667c135
fm <francesco.magistrali@mail.polimi.it>
2021-04-04 19:09:19 +0200

    classes structure

--- src/main/java/it/polimi/ingsw/ResourceStructure.java
+++ src/main/java/it/polimi/ingsw/ResourceStructure.java
@@ -1,20 +1,48 @@
 package it.polimi.ingsw;
 import java.util.ArrayList;
 public class ResourceStructure {
-    private ArrayList<Character> vector = new ArrayList<Character>();
-    public ArrayList<Character> getVector() { return vector; }
 
+    private ArrayList<Character> vector = new ArrayList<Character>();
+
+
+    // ritorna l'arraylist in questione
+    public ArrayList<Character> getVector() {
+        return vector;
+    }
+
+    // setta l'arraylist in questione in base all'array che gli si passa
     public void setVector(ArrayList<Character> vector) {
         this.vector = vector;
     }
 
+
     public ArrayList<Character> Order(ArrayList<Character> vector)
     {
+        // da implementare ?
         return vector;
     }
 
-    public ArrayList<Character> AddResource(int quantity, Character resource){
+    // controlla che un array list sia composto da una sola risorsa
+    public boolean checkEquality(ArrayList<Character> vector) {
+        int counter = 0;
+        Character first = vector.get(0);
+        for(int i=0; i <= vector.size(); i++) {
+            if (vector.get(i) == first) {
+                counter ++;
+            }
+        }
+    if (counter == vector.size() + 1) {
+        return true;
+    } else {
+        return false;
+    }
+
+    }
+
+
+    public ArrayList<Character> AddResource (int quantity, Character resource) {
         for(int i=0; i<quantity; i++) vector.add(resource);
         return(vector);
     }
+
 }
--- src/main/java/it/polimi/ingsw/Storage.java
+++ src/main/java/it/polimi/ingsw/Storage.java
@@ -1,22 +1,56 @@
 package it.polimi.ingsw;
 
 public class Storage {
-    private Resource firstfloor;
-    private ResourceStructure secondfloor;
-    private ResourceStructure thirdfloor;
-    private ResourceStructure additionalfloor;
+            private ResourceStructure panel; // arraylist di 6 spazi di base
+            private ResourceStructure extrapanel;
 
 
-    public boolean getResource(int number, Resource withdraw) {
-        return boolean ;
-    }
 
-    public void setResource(int number, Resource deposit) {
+        // controlla se sono presenti nel panel tot risorse di uno specifico tipo
+        public boolean getinStorage (int quantity, Character type) {                    // OK!!!!
+           int counter = 0;
+           for (int i=0; i < 6; i ++) {
+               if (this.panel.get(i) == type) {
+               counter ++;
+                                                 }
+           }
+           if (counter == quantity && this.checkStorage() == true) {
+               return true;
+           } else return false;
+        }
 
-    }
 
-    public int counterResource() {
-    }
+
+        // inserisce nel panel una data risorsa (se si può)
+        public void setinStorage (Character type, int position) {
+
+            if (getinStorage(1, type) == true) {
+                this.panel.set(position, type) = type; // assengnazione giusta?
+            } else this.extrapanel.add(type) = true;  // assegnazione giusta?
+        }
+
+
+        // se viene lanciata un'exeption in setinstorage oppure in getinstorage si attiva e modifica arraylist
+        public void repairStorage () {
+            // TOSTA IN CULO CANE PORCO
+
+        }
+
+        // controlla se i sei spazi del panel rispettano le regole prefissate del gioco se non le rispetta chiama repairstorage
+        public boolean checkStorage () {
+            if (    this.panel.get(0) != this.panel.get(1) &&
+                    this.panel.get(1) != this.panel.get(3) &&
+                    this.panel.get(0) != this.panel.get(3) &&
+                    this.panel.get(1) == this.panel.get(2) &&
+                    this.panel.get(3) == this.panel.get(4) &&
+                    this.panel.get(4) == this.panel.get(5)         ) // però possono essere anche vuoti! come posso eprimerlo?
+            { return true; } else {
+                                this.repairStorage();
+                                return false; }
+        }
+
+        // controlla le risorse extra che vanno ad incrementare i puntifede degli altri giocatori
+        public int returnmoves () {
+            return this.extrapanel.size();
+        }
 }
-
-
