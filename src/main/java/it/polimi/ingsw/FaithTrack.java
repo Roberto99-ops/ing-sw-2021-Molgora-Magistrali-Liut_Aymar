d3892617a58419d650b16c4f5a4213bb8fc5b418
juan <juandavid.liut@mail.polimi.it>
2021-04-05 09:53:29 +0200

    minor changes

--- src/main/java/it/polimi/ingsw/FaithTrack.java
+++ src/main/java/it/polimi/ingsw/FaithTrack.java
@@ -2,72 +2,52 @@ package it.polimi.ingsw;
 
 public class FaithTrack {
     private int position;
-    private int PV; //a seconda di dove si trova il segnalino riceve tot PV: ex cella #17==9PV
-                    //Partirebbe da 0 così mentre avanza "raccoglie" i PV sul tracciato
-                    //oppure si può usare il PV di PLayer?
-    private int VR;
+    private int PV_Track=0; //a seconda di dove si trova il segnalino riceve tot PV: ex cella #17==9PV
+    //Partirebbe da 0 così mentre avanza "raccoglie" i PV sul tracciato
+    //oppure si può usare il PV di PLayer?
 
     //prende la cella del tracciato in cui si trova il segnalino Fede
-    public int GetPosition() {
+    public int GetPosition(){
         return position;
     }
 
-    // ritorna il numero di PV in relazione alla posizione
-    public int getPV (int position) {
-        if (this.position < 3) {
-            return 0;
-        } else if (this.position > 2 && this.position < 6) {
-            return 1;
-        } else if (this.position > 5 && this.position < 9) {
-            return 2;
-        } else if (this.position > 8 && this.position < 12) {
-            return 4;
-        } else if (this.position > 11 && this.position < 15) {
-            return 6;
-        } else if (this.position > 14 && this.position < 18) {
-            return 9;
-        } else if (this.position > 17 && this.position < 21) {
-            return 12;
-        } else if (this.position > 20 && this.position < 24) {
-            return 16;
-        } else if (this.position == 24) {
-           // Game.this.Endgame();
-            return 20;
-            /*
-            Game.this.Endgame();
-            é giusto scriverli così?
-            dove li metto? in quale metodo? perchè potrei anche
-            metterlo in vaticanreport oppure in incrementposittion
-             */
-        } else {
-            return 25; // exception
-        }
-    }
-
     //verifica che il segnalino si trovi nell'area VaticanReport
-    public void vaticanReport() {
-        if (this.position < 5) {
-            this.VR=0;
-        } else if (this.position > 4 && this.position < 9) {
-            this.VR=1;
-        } else if (this.position > 11 && this.position < 17) {
-            this.VR=2;
-        } else if (this.position > 18) {
-            this.VR = 3;
-        } else {
-            this.VR = 4; //exception
+    public void VaticanReport(int position){
+        int i;
+        int VR = Player.getVR(); // VR = vatican report
+        // caso in cui un giocatore arrivi per primo a fare il VR
+        if ((position>=8 && position<=15)&& VR==0){
+            PV_Track+=2; //il VR della prima area vale 2
+            //controllo che i giocatori si trovino nell'area del VR e se lo sono sommo i punti
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>4 && Player/*numero*/.trackposition<9){
+                    Player/*numero*/.PV+=2;
+                }
+            }
+            Player.setVR(1); //setta il VR di tutti i giocatori a 1 perchè la prima area è stata superata
+        } else if ((position>=16 && position<=23)&& VR==1){
+            PV_Track+=3; // seconda area vale 3
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>11 && Player/*numero*/.trackposition<17){
+                    Player/*numero*/.PV+=3;
+                }
+            }
+            Player.setVR(2);
+        } else if ((position==24)&& VR==1){
+            PV_Track+=4; // terza area vale 4
+            for (i=0; i<3; i++){
+                if (Player/*numero*/.trackposition>18) {
+                    Player/*numero*/.PV=+ 4;
+                }
+            }
+            //chiama EndGame() e Victory()
+            Game.EndGame(); //chiama poi victory()
         }
+
     }
 
-    public int GetVR() {
-        return VR;
+    public void SetPosition(int position){
+        this.position=position;
     }
 
-    public void SetPosition(int pos) {
-        this.position = pos;
-    }
-
-    public void incrementPosition() {
-        this.position++;
-    }
 }
