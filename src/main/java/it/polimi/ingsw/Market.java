1223c9e92f771a3b5ec325c41058099189e2dce5
fm <francesco.magistrali@mail.polimi.it>
2021-04-07 20:40:46 +0200

    news

--- src/main/java/it/polimi/ingsw/Market.java
+++ src/main/java/it/polimi/ingsw/Market.java
@@ -1,16 +1,74 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class Market {
-    private Character matrix[][];
-    private Character extraball;
+    private char[][] matrix = new char[3][4];
+    private char extraball;
 
-    //prende le risorse dalla matrice
-    public Character GetResource (boolean check, int number) {
+
+    // metodo che randomizza la matrix?
+
+
+    /*
+    inserisce la extraball nella matrice + nuova extraball
+    check mi dice se è colonna/riga da sopra/sotto
+    il metodo ritorna arraylist con le
+     */
+     */
+
+    public  ArrayList<Character> doMarket(int check, int number) {
+        char temporaryball = this.extraball;
+        char[][] equalmatrix = new char[3][4];
+        equalmatrix = this.matrix;
+        ArrayList<Character> vector = new ArrayList<Character>();
+
+        if (check == 1) {
+            for (int i = 0; i < 2; i++) {
+                this.matrix[i + 1][number] = equalmatrix[i][number];
+                i++;
+                vector.add(equalmatrix[i][number]);
+            }
+            this.matrix[0][number] = temporaryball;
+            this.extraball = equalmatrix[2][number];
+            vector.add(equalmatrix[2][number]);
+        }
+
+        if (check == 1) {
+            for (int i = 0; i < 2; i++) {
+                this.matrix[2-i][number] = equalmatrix[2-i][number];
+                i++;
+                vector.add(equalmatrix[2-i][number]);
+            }
+            this.matrix[2][number] = temporaryball;
+            this.extraball = equalmatrix[0][number];
+            vector.add(equalmatrix[0][number]);
+        }
+
+        if (check == 3) {
+            for (int i = 0; i < 3; i++) {
+                this.matrix[number][i + 1] = equalmatrix[number][i];
+                i++;
+                vector.add(equalmatrix[number][i]);
+            }
+            this.matrix[number][0] = temporaryball;
+            this.extraball = equalmatrix[number][3];
+            vector.add(equalmatrix[number][3]);
+        }
+
+        if (check == 4) {
+            for (int i = 0; i < 3; i++) {
+                this.matrix[number][3-i] = equalmatrix[number][3-i];
+                i++;
+                vector.add(equalmatrix[number][3-i]);
+            }
+            this.matrix[number][3] = temporaryball;
+            this.extraball = equalmatrix[number][0];
+            vector.add(equalmatrix[number][0]);
+        }
 
     }
 
-    //inserisce la extraball nella matrice + nuova extraball
-    public void SetResource ( boolean check, int number ){
+    return vector;
 
-    }
 }
--- src/main/java/it/polimi/ingsw/StrongBox.java
+++ src/main/java/it/polimi/ingsw/StrongBox.java
@@ -5,16 +5,24 @@ public class StrongBox {
         private ResourceStructure structure;
 
 
-    public boolean getResource (int number, Resource withdraw) {
-        return boolean;
+    public boolean getResource (int number, char withdraw) {
+        int counter = 0 ;
+            for (int i=0 ; i <= number; i++ ) {
+                if (this.structure.getVector().get(i) == withdraw) {
+                    counter++;
+                }
+            }
+
+            if (counter == number) {
+                return true; } else { return false; }
     }
 
-    public void setResource (int number, Resource deposit) {
-
+    public void setResource (int number, char deposit) {
+        for (int i=0 ; i <= number; i++) {
+            this.structure.getVector().add(deposit);
+        }
     }
 
     public int counterResource () {
-
+        return this.structure.getVector().size();
     }
-
-}
