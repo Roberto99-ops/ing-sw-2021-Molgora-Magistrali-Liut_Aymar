68d7e701ea47ea8df170db0fcb23c47cef594870
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-04-08 19:29:35 +0200

    Turn implemented, it needs some implementations in other classes to work

--- src/main/java/it/polimi/ingsw/Turn.java
+++ src/main/java/it/polimi/ingsw/Turn.java
@@ -1,8 +1,8 @@
 package it.polimi.ingsw;
+import java.io.IOException;
 
 public class Turn {
     private Player actualplayer;
-    private int action;
 
     public Player getActualplayer() {
         return actualplayer;
@@ -12,16 +12,95 @@ public class Turn {
         this.actualplayer = actualplayer;
     }
 
+    /**
+     * turn "main", it manages all the turn.
+     * 1)asks us what do we want to do
+     * 2)in case we choose to active a production, we join a while that active all the production we want
+     *      2.1)it lists us all our LeaderCards so we can choose to active one of that
+     *      2.2) do the same with our Developecards
+     * then the turn ends
+     */
+    public void main()
+    {
+        int action=0;
+        char again='n';
+        int card=0;
 
-    private DevelopeCard ShopCard() {
+        //1)
+        System.out.println("What do you want to do?\n\t1)Shop a developement card\n\t2)Take resources at the market\n\t3)Active a production\n");
+        try{ action = System.in.read();
+            if(action == 1) this.ShopCard();
+            if(action == 2) this.Buyresource();
+            //2)
+            if(action == 3) { do{//2.1)
+                                System.out.println("Which LeaderCard do you want to enable(0=none)?\n");
+                                this.actualplayer.getLeadercards().Print();
+                                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
+                                if(card!=0) this.Production(this.actualplayer.getLeadercards().getStructure().get(card));
+
+                                //2.2)
+                                System.out.println("Which DevelopeCard do you want to enable(0=none)?\n");
+                                this.actualplayer.getDevelopecards().Print(); //non so come il player accede alle proprie carte sviluppo
+                                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
+                                if(card!=0) this.Production(this.actualplayer.getLeadercards().getStructure().get(card));
+
+                                System.out.println("Do you want to do another production(y/n)?\n");
+                                try{ again = (char) System.in.read(); } catch (IOException e){ System.out.println(e);} }while(again=='y');}
+        } catch (IOException e){ System.out.println(e);}
 
     }
 
-    public ResourceStructure Buyresource(boolean choice, int quantity) {
+    /**
+     * this method manages the purchase of a DevelopeCard.
+     * the class asks to the costumer what card he do want to buy
+     * then check if he own enought resources and complete the transaction
+     */
+    private void ShopCard() {
+        int choose=0;
+        DevelopeCard card = new DevelopeCard();
+        System.out.println("Choose the number of the card you want to buy ");
+        try{ choose = (char) System.in.read(); } catch (IOException e){ System.out.println(e);}
+        card.equals(game.getDevelopeCard(choose));
 
+        if(!this.actualplayer.getResources().cotains(card.getCost())) return;
+
+        this.actualplayer.setResources(this.actualplayer.getResources().remove(card.getCost()));
+        this.actualplayer.getDevelopeCards.add(card);
     }
 
-    private void Production(DevelopeCard carta) {
+    /**
+     * this method manages the action of buying resources at the market.
+     * the player choose which row/column and call the manager market method
+     * @return
+     */
+    public ResourceStructure Buyresource() {
+        ResourceStructure product = new ResourceStructure();
+        int RoworCol = 0;
+        int number = 0;
 
+        System.out.println("Do you want to choose a row or a column?\n\t1)Row\n\t2)Column\n");
+        try{ RoworCol = System.in.read(); } catch (IOException e){ System.out.println(e); }
+
+        if(RoworCol == 1)
+            System.out.println("Wich row do you want to take?\n");
+        if(RoworCol == 2)
+            System.out.println("Wich column do you want to take?\n");
+        try{ number = System.in.read(); } catch (IOException e){ System.out.println(e); }
+
+        product.setVector(game.getMarket.doMarket(RoworCol, number)); //???
+
+        return product;
+    }
+
+    /**
+     * active the producition of the given card
+     * @param card
+     */
+    private void Production(DevelopeCard card) {
+        this.actualplayer.setResources(card.Production(this.actualplayer.getResources,card));
+    }
+
+    private void Production(LeaderCard card) {
+        this.actualplayer.setResources(card.Production(this.actualplayer.getResources,card));
     }
 }
