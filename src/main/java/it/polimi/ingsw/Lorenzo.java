47d517fb2095fc7d6340ee4d1719cb687a6eba54
Juan David Liut Aymar <juandavid.liut@mail.polimi.it>
2021-04-09 13:56:55 +0200

    -fixed method 'LorenzoMoves' -added JavaDoc for method 'LorenzoMoves'

--- src/main/java/it/polimi/ingsw/Lorenzo.java
+++ src/main/java/it/polimi/ingsw/Lorenzo.java
@@ -4,13 +4,18 @@ public class Lorenzo {
     private String name = "Lorenzo Il Magnifico";
     private int number = 0; //posizione su tracciato
 
+    /**
+     * Moves Lorenzo forward by 1 or 2 cells on the FaithTrack, according to the picked Signal
+     * @param forward
+     * @return
+     */
     public int Lorenzomoves(int forward){
+        ActionStructure Stack = new ActionStructure();
         if (forward==2){
             number+=2;
         } else if (forward==1){
             number++;
-            //chiamo metodo Shuffle per ActionStructure qui?
-            ActionStucture.ShuffleSignal();
+            Stack.ShuffleSignal();
         }
         return number;
     }
