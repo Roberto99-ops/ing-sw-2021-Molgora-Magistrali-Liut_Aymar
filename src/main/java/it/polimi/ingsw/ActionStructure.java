2071f8f6c7e69714ab537d98d674fb3645885b07
Juan David Liut Aymar <juandavid.liut@mail.polimi.it>
2021-04-09 13:43:39 +0200

    -changed type of attribute 'structure': from 'ActionStructure' to 'int' -fixed method 'PickSignal' -added JavaDoc for all methods

--- src/main/java/it/polimi/ingsw/ActionStructure.java
+++ src/main/java/it/polimi/ingsw/ActionStructure.java
@@ -1,12 +1,19 @@
 package it.polimi.ingsw;
 
 public class ActionStructure {
-    private ActionSignal[] structure;
+    private int[] structure;
     private int AS_Counter=0; //ActionSignal_counter conta il segnalino che è stato preso (da 0 a 6 -> posti nella pila)
 
-    //metodo che prende il primo segnalino della pila e lo rimette in fondo
-    public int PickSignal(ActionStructure[] structure, int AS_Counter){
-        ActionSignal.Action(structure[AS_Counter]); //chiamo il metodo del segnalino in base al suo numero
+    /**
+     * Picks the first Signal available on the stack, calls its action and puts it back, at the
+     * end of the stack
+     * @param structure
+     * @param AS_Counter
+     * @return
+     */
+    public int PickSignal(int[] structure, int AS_Counter){
+        ActionSignal Signal = new ActionSignal();
+        Signal.Action(structure[AS_Counter]); //chiamo il metodo del segnalino in base al suo numero
         AS_Counter++;
         if (AS_Counter==7){ //se arrivo alla fine della pila torno su, al posto 0
             AS_Counter=0;
@@ -14,7 +21,9 @@ public class ActionStructure {
         return AS_Counter;
     }
 
-    //metodo che mischia le carte del mazzo
+    /**
+     * Shuffles the Signals' stack
+     */
     public void ShuffleSignal(){
         Game theGame = new Game();
         theGame.Shuffle();
