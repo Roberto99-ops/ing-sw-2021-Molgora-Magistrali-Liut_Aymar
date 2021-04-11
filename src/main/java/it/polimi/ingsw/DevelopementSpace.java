a2cbd7a95df2032c0ffb1ee02a6ae5d8bb5cc891
Juan-LA <10607787@polimi.it>
2021-04-11 16:28:18 +0200

    Revert "no message"
    This reverts commit 527195796663e77ca81b90ae7e1476724fe04300.

--- src/main/java/it/polimi/ingsw/DevelopementSpace.java
+++ src/main/java/it/polimi/ingsw/DevelopementSpace.java
@@ -8,7 +8,6 @@ public class DevelopementSpace {
 
 
 <<<<<<< HEAD
-<<<<<<< HEAD
     public DevelopeCard getCard(int choice) {
         int i = 0;
         if (choice == 1) {
@@ -75,16 +74,6 @@ public class DevelopementSpace {
         }
 
     }
-=======
-    public DevelopeDecks getCard() {
-        return structure;
-    }
-
-    public void setCard(DevelopeCard structure) {
-        this.structure = structure;
-
-    }
->>>>>>> parent of fb965f5 ( news by Fra :))
 =======
     public DevelopeDecks getCard() {
         return structure;
