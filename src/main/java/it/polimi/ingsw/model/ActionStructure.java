3e9dfb0fd328c8449c33386bde66c272d062b054
juan <juandavid.liut@mail.polimi.it>
2021-04-22 11:49:14 +0200

    - imported util.Array and .Collections - minor changes

--- src/main/java/it/polimi/ingsw/model/ActionStructure.java
+++ src/main/java/it/polimi/ingsw/model/ActionStructure.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ActionStructure {
     private int[] structure;
     private int AS_Counter=0; //ActionSignal_counter conta il segnalino che è stato preso (da 0 a 6 -> posti nella pila)
@@ -7,11 +10,9 @@ public class ActionStructure {
     /**
      * Picks the first Signal available on the stack, calls its action and puts it back, at the
      * end of the stack
-     * @param structure
-     * @param AS_Counter
      * @return
      */
-    public int PickSignal(int[] structure, int AS_Counter){
+    public int PickSignal(){
         ActionSignal Signal = new ActionSignal();
         Signal.Action(structure[AS_Counter]); //chiamo il metodo del segnalino in base al suo numero
         AS_Counter++;
@@ -25,10 +26,7 @@ public class ActionStructure {
      * Shuffles the Signals' stack
      */
     public void ShuffleSignal(){
-        Game theGame = new Game();
-        theGame.Shuffle();
-        //oppure ho trovato questo metodo per mischiare un array usando libreria standard di Java
-        //Collections.shuffle(Arrays.asList(structure))
+        Collections.shuffle(Arrays.asList(structure));
         AS_Counter=0;
     }
 
