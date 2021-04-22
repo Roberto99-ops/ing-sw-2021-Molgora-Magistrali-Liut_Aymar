ae5b68b9f9025ce461f8b978924ab4068bc14822
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-04-22 19:14:27 +0200

    listed some simbols that could be useful

---
+++ src/main/java/it/polimi/ingsw/view/cli/Simbol.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.view.cli;
+
+public enum Simbol {
+    CALAMAIO("\u0238"), // u0444 u0460  u03A6
+    CINQUE("\u168E"),
+    MIRINO("\u1AA0"), //?
+    IX("\u1CC1"),  //?
+    GRECHINA("\u2021"),
+    ARZIGOGOLO("\u2318"),
+    BITCOIN("\u20BF"),
+    FRECCIA_SU("\u2191"),
+    FRECCIA_GIU("\u2193"),
+    FRECCIA_DX("\u2192"),
+    FRECCIA_SX("\u2190"),
+    CLESSIDRA1("\u23F3"),
+    CLESSIDRA2("\u231B"),
+    OROLOGIO("\u231A"),
+    TASTIERA("\u2328"),
+    FACCIA_FELICE("\u263B"),
+    FACCIA_TRISTE("\u2639"),
+    SEGNALINO_PAPALE("\u2657"),  //265D
+    PALLINO("\u25CF");
+
+    private String form;
+
+    Simbol(String form)
+    {
+        this.form = form;
+    }
+
+
+    public String getForm()
+    {
+        return form;
+    }
+
+    @Override
+    public String toString()
+    {
+        return form;
+    }
+}
