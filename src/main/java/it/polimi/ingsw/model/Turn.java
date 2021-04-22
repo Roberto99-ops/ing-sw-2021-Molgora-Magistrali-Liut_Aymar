fc40c06f5a94ca3d1af9ac5ede946b56e8597a6b
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-04-22 13:08:49 +0200

    buyresource e shopcard implemented and fixed

--- src/main/java/it/polimi/ingsw/model/Turn.java
+++ src/main/java/it/polimi/ingsw/model/Turn.java
@@ -5,7 +5,7 @@ public class Turn {
     private Player actualplayer;
 
     private Game game;
-    private int l= game.getLonely();
+    private int l = game.getLonely();
     private ActionStructure aStructure;
 
     public Player getActualplayer() {
@@ -20,45 +20,47 @@ public class Turn {
      * turn "main", it manages all the turn.
      * 1)asks us what do we want to do
      * 2)in case we choose to active a production, we join a while that active all the production we want
-     *      2.1)it lists us all our LeaderCards so we can choose to active one of that
-     *      2.2) do the same with our Developecards
+     * 2.1)it lists us all our LeaderCards so we can choose to active one of that
+     * 2.2) do the same with our Developecards
      * then the turn ends
      */
     public void main() {
-        int action=0;
-        char again='n';
-        int card=0;
+        int action = 0;
+        char again = 'n';
+        int card = 0;
 
         //Ad ogni turno, effettuo il controllo del Vatican Report
         FaithTrack faithTrack = new FaithTrack();
         faithTrack.VaticanReport();
 
         //se sono in single game, ogni volta che tocca a me, prendo un segnalino ed eseguo la sua azione
-        if (l==1){
+        if (l == 1) {
             ActionSignal.Action(aStructure.PickSignal());
         }
 
         //1)
         System.out.println("What do you want to do?\n\t1)Shop a developement card\n\t2)Take resources at the market\n\t3)Active a production\n");
-        try{ action = System.in.read();
-            if(action == 1) this.ShopCard();
-            if(action == 2) this.Buyresource();
+        try {
+            action = System.in.read();
+            if (action == 1) this.ShopCard(game);
+            if (action == 2) this.Buyresource(game);
             //2)
             if(action == 3) { do{//2.1)
-                                System.out.println("Which LeaderCard do you want to enable(0=none)?\n");
-                                this.actualplayer.getLeadercards().Print();
-                                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
-                                if(card!=0) this.Production(this.actualplayer.getLeadercards().getStructure().get(card));
+                System.out.println("Which LeaderCard do you want to enable(0=none)?\n");
+                this.actualplayer.getLeadercards().Print();
+                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
+                if(card>0) this.actualplayer.getLeadercards().getStructure().get(card-1).Skill();
 
-                                //2.2)
-                                System.out.println("Which DevelopeCard do you want to enable(0=none)?\n");
-            //da decommentare                    this.actualplayer.getDevelopecards().Print(); //non so come il player accede alle proprie carte sviluppo
-                                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
-                                if(card!=0) this.Production(this.actualplayer.getLeadercards().getStructure().get(card));
+                //2.2)
+                System.out.println("Which DevelopeCard do you want to enable(0=none)?\n");
+                this.actualplayer.getDevelopecards().Print(); //non so come il player accede alle proprie carte sviluppo
+                try{ card = System.in.read(); } catch (IOException e){ System.out.println(e);}
+                if(card>0) this.actualplayer.getDevelopecards().getStructure().get(card-1).DoProduction(this.actualplayer);
 
-                                System.out.println("Do you want to do another production(y/n)?\n");
-                                try{ again = (char) System.in.read(); } catch (IOException e){ System.out.println(e);} }while(again=='y');}
+                System.out.println("Do you want to do another production(y/n)?\n");
+                try{ again = (char) System.in.read(); } catch (IOException e){ System.out.println(e);} }while(again=='y');}
         } catch (IOException e){ System.out.println(e);}
+
 
     }
 
@@ -67,54 +69,67 @@ public class Turn {
      * the class asks to the costumer what card he do want to buy
      * then check if he own enought resources and complete the transaction
      */
-    private void ShopCard() {
-        int choose=0;
-        Game game=new Game();
+    private void ShopCard(Game game) {
+        int choose = 0;
         DevelopeCard card = new DevelopeCard();
+        ResourceStructure newcost = new ResourceStructure();
         System.out.println("Choose the number of the card you want to buy ");
-        try{ choose = (char) System.in.read(); } catch (IOException e){ System.out.println(e);}
-        //card.equals(game.getCard(choose));  -> questo meth non esiste. Era forse getCard di DevelopeCard?
+        try {
+            choose = (char) System.in.read();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        card.equals(game.getDevelopedecks()[choose].getStructure().get(0));
 
-    //da decommentare    if(!this.actualplayer.getResources().cotains(card.getCost())) return;
+        if (!this.actualplayer.getResources().getVector().contains(card.getCost())) return;
 
-    //da decommentare    this.actualplayer.setResources(this.actualplayer.getResources().remove(card.getCost()));
-    //da decommentare    this.actualplayer.getDevelopeCards.add(card);
+        for (int i = 0; i < this.actualplayer.getLeadercards().getStructure().size(); i++)
+            if (this.actualplayer.getLeadercards().getStructure().get(i).getNumber() < 4)
+                newcost = this.actualplayer.getLeadercards().getStructure().get(i).Skill();
+
+        this.actualplayer.getResources().getVector().remove(newcost);
+
+        this.actualplayer.getDevelopecards().getStructure().add(card);
     }
 
     /**
      * this method manages the action of buying resources at the market.
      * the player choose which row/column and call the manager market method
+     *
+     * @param game
      * @return
+     * @throws IOException
      */
-    public ResourceStructure Buyresource() {
+    public ResourceStructure Buyresource(Game game) throws IOException {
         ResourceStructure product = new ResourceStructure();
+        Market market = game.getMarket();
         int RoworCol = 0;
         int number = 0;
 
         System.out.println("Do you want to choose a row or a column?\n\t1)Row\n\t2)Column\n");
-        try{ RoworCol = System.in.read(); } catch (IOException e){ System.out.println(e); }
+        try {
+            RoworCol = System.in.read();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
-        if(RoworCol == 1)
+        if (RoworCol == 1)
             System.out.println("Wich row do you want to take?\n");
-        if(RoworCol == 2)
+        if (RoworCol == 2)
             System.out.println("Wich column do you want to take?\n");
-        try{ number = System.in.read(); } catch (IOException e){ System.out.println(e); }
+        try {
+            number = System.in.read();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
-    //da decommentare    product.setVector(game.getMarket.doMarket(RoworCol, number)); //???
+        product.setVector(game.getMarket().doMarket(RoworCol, number));
+
+        for (int i = 0; i < this.actualplayer.getLeadercards().getStructure().size(); i++)
+            if (this.actualplayer.getLeadercards().getStructure().get(i).getNumber() < 12 && this.actualplayer.getLeadercards().getStructure().get(i).getNumber() > 7)
+                product = this.actualplayer.getLeadercards().getStructure().get(i).Skill();
 
         return product;
     }
 
-    /**
-     * active the production of the given card
-     * @param card
-     */
-    private void Production(DevelopeCard card) {
-    //da decommentare    this.actualplayer.setResources(card.Production(this.actualplayer.getResources,card));
-    }
-
-    private void Production(LeaderCard card) {
-    //    this.actualplayer.setResources(card.Production(this.actualplayer.getResources,card));
-    }
-
-    }
+}
