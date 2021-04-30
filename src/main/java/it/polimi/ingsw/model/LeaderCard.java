1a5332061601b3a03844f346a9b07dbe4a1fe5e3
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-04-30 22:21:01 +0200

    fixed all the skills methods and added the skill assignement into setcard method

--- src/main/java/it/polimi/ingsw/model/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/model/LeaderCard.java
@@ -67,14 +67,12 @@ public class LeaderCard{
         FileReader stringa = new FileReader("src/main/resources/LeaderCards.json");
         Object obj = JsonParser.parseReader(stringa);
         JsonObject jsonObject = (JsonObject) obj;
-        //int num = jsonObject.get("ciao").getAsInt();
-        //System.out.println(num);
         JsonArray cardsArray = (JsonArray) jsonObject.get("LeaderCards");
         JsonObject card = (JsonObject) cardsArray.get(number);
 
         this.cardLevel = card.get("cardLevel").getAsInt();
-        //this.skill = card.get("skill").getAsString();
-        this.inputskill = card.get("skill").getAsCharacter();
+        this.skill = card.get("skill").getAsString();
+        this.inputskill = card.get("inputskill").getAsCharacter();
         this.pv = card.get("pv").getAsInt();
 
         size = card.get("priceR").getAsJsonArray().size();
@@ -94,88 +92,101 @@ public class LeaderCard{
     //OPPURE mi sa che è meglio semplicemente trattare tutti i casi nella classe player o turno --> perciò attributo
     //skill delle carte diventa un char
     //number è il numero della carta ovvero la sua posizione nell'array
-    public ResourceStructure Skill()
+   /* public ResourceStructure Skill()
     {
-        DevelopeCard card=new DevelopeCard();
         try {
             if (this.number < 12) {
                 if (this.number < 8) {
                     if (this.number < 4) {
-    //da sistemare                    return this.PriceSkill(boh, this.skill);
+                        return this.PriceSkill(boh, this.skill);
                     }
                     return this.AdditionalStorageSkill(this.inputskill);
                 }
-    //da sistemare           return this.ConvertWhiteMarbleSkill(this.skill, boh);
+               return this.ConvertWhiteMarbleSkill(this.skill, boh);
             }
-   //da sistemare         return this.AdditionalProductionSkill(this.skill, boh);
+            return this.AdditionalProductionSkill(this.skill, boh);
         } catch(Exception e){System.out.println("Inexistent LeaderCard");}
         return null;
     }
-
+/*
 
     /**
      * this skill decreases the develope card price of a specify resource
-     * @param card
-     * @param resource
-     * @return
+     * @param card: card in input to decrease price
+     * @return: the "new" cost
      */
-    public ResourceStructure PriceSkill(DevelopeCard card, char resource)
+    public ResourceStructure PriceSkill(DevelopeCard card)
     {
         ResourceStructure newCost = new ResourceStructure();
-        if(!card.getCost().getVector().contains(resource))  return card.getCost();
-        newCost.equals(card.getCost().getVector().remove(resource));
+        if(!card.getCost().getVector().contains(this.inputskill))  return card.getCost();
+        newCost.equals(card.getCost().getVector().remove(this.inputskill));
         return newCost;
     }
 
     /**
-     * this skill give to the player the type fo resource that can be
-     * contained in the additional storage layer
-     * @param resource
-     * @return
+     * this skill activate the additional storage layer by specifying the type of resource it can contain
+     * @param player: player that get the additional layer
      */
-    public ResourceStructure AdditionalStorageSkill(char resource)
+    public void AdditionalStorageSkill(Player player)
     {
-        ResourceStructure resourceType = new ResourceStructure();
-        resourceType.AddResource(1, resource);
-        return resourceType;   //deve mettere a 1 il flag che dice che il player ha un piano in più nello storage
+        player.getStorage().setTypeExtrapanel(this.inputskill);
     }
+
 
     /**
      * this skill "replace" white marbles in the row/column chosen, with marbles of a given colour
-     * @param resource
-     * @param row
-     * @return
+     * @param row: is the row of the market containing marbles
+     * @return: the "new" row, where white marbles are substituted by marbles of the given color
      */
-    public ResourceStructure ConvertWhiteMarbleSkill(char resource, ResourceStructure row)
+    public ResourceStructure ConvertWhiteMarbleSkill(ResourceStructure row)
     {
         while(row.getVector().contains('W'))
         {
             row.getVector().remove('W');
-            row.getVector().add(resource);
+            row.getVector().add(this.inputskill);
         }
         return row;
     }
 
-    //non so come abbiamo implementato i punti fede quindi per
-    //ora li tratto solo come una risorsa rossa
-    //non so se devo dargli in ingresso anche il baule o storage in modo da togliere quella risorsa
-
     /**
-     * this skill give us an additional production we can do
-     * @param resource
-     * @param storage
-     * @return
+     * this method do a production.
+     * (1)checks if the player owns enough resources to activate the production;
+     * (2)then if storage contains enough resources, it removes from there the input resources and add the output resources to the strongbox;
+     * (3)if the storage doesn't contain enough resources it does the same thing with the strongbox.
+     * it uses the (0,1,2) logic defined into the player.checkresources method to check where the resources are.
+     * @param player: is the player who wants to do a production
      */
-    public ResourceStructure AdditionalProductionSkill(char resource, ResourceStructure storage) throws IOException {
-        char resourceChosen;    //storage in ingresso
-        if(!storage.getVector().contains(resource)) return null;
-        storage.getVector().remove(resource);
-        ResourceStructure production = new ResourceStructure();
-        production.AddResource(1, 'R');
-        System.out.println("What resource do you want to produce?\n(B, G, Y, P)\t");
-        resourceChosen = (char)System.in.read();
-        production.AddResource(1, resourceChosen);
-        return production;  //copiare quello di developecard e al posto della risorsa rossa fare player.setposition++
+    public void AdditionalProductionSkill(Player player) throws IOException {
+
+        char resourcechosen;
+        ResourceStructure resource = new ResourceStructure();
+        resource.add(this.inputskill);
+
+        //(1)
+        if(player.CheckResources(resource)!=0) {
+
+            player.increaseTrackposition();
+            System.out.println("What resource do you want to produce?\n(B, G, Y, P)\t");
+            resourcechosen = (char)System.in.read();
+
+            //(2)
+            if(player.CheckResources(resource)==1)
+            {
+                player.removeResource(this.inputskill);
+
+                player.addResourceStrongBox(resourcechosen); }
+
+            //(3)
+            if(player.CheckResources(resource)==2)
+            {
+                if(player.getStorage().getPanel().contains(this.inputskill))
+                        player.removeResource(this.inputskill);
+                else
+                    player.getStrongBox().deleteResource(this.inputskill);
+
+                player.addResourceStrongBox(resourcechosen); }
+        }
+        else System.out.println("You don't own enought Resources");
     }
 
 
