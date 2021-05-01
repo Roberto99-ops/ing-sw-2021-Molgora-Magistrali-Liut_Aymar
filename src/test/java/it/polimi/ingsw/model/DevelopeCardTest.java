143275bb007bad1cf6c03d575885dfaa6f0ac0ef
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-05-01 02:25:55 +0200

    nothing changed

--- src/test/java/it/polimi/ingsw/model/DevelopeCardTest.java
+++ src/test/java/it/polimi/ingsw/model/DevelopeCardTest.java
@@ -3,13 +3,46 @@ package it.polimi.ingsw.model;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.lang.management.PlatformLoggingMXBean;
 
 import static org.junit.Assert.*;
 
 public class DevelopeCardTest {
 
+    /**
+     * test the DoProduction method.
+     * (1) tests the case which the inputresources are all inside the storage
+     * (2) tests the case which the inputresources are all inside the strongbox
+     * (3) tests the case which the inputresources are divided by storage and strongbox
+     * (4) tests the case which the player doesn't own enough resources
+     * @throws FileNotFoundException
+     */
     @Test
-    public void doProduction() {
+    public void doProduction() throws FileNotFoundException {
+       /* DevelopeCard card = new DevelopeCard();
+        card.setCard(21);
+
+        //(1)
+        Player player = new Player();
+        ResourceStructure output = new ResourceStructure();
+        player.addResourceStorage('B');
+        player.addResourceStorage('P');
+        output.AddResource(3, 'W');
+        card.DoProduction(player);
+        assertEquals(null, player.getStorage());
+        assertTrue(player.getStorage().getinStorage(3, 'W'));
+
+        //(2)
+        Player player1 = new Player();
+        ResourceStructure output1 = new ResourceStructure();
+        player.addResourceStrongBox('B');
+        player.addResourceStrongBox('P');
+        output1.AddResource(3, 'W');
+        card.DoProduction(player1);
+        for(int i=0; i<player1.getStorage().size(); i++)    assertEquals(output1.getVector().get(i), player1.getStrongBox().getStructure().getVector().get(i));
+
+
+        */
     }
 
     /**
