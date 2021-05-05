c7f7976a509d1547c5e455dcb8a2eef4313ec73e
juan <juandavid.liut@mail.polimi.it>
2021-05-05 18:19:11 +0200

    - added instances and their getters and setters to SingleGame class - added method 'main' (its body is still not finished)

--- src/main/java/it/polimi/ingsw/model/SingleGame.java
+++ src/main/java/it/polimi/ingsw/model/SingleGame.java
@@ -1,15 +1,124 @@
 package it.polimi.ingsw.model;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SingleGame extends Game {
-    private Lorenzo Lore = new Lorenzo();
+    //cosa ha:
+    private static ArrayList<Player> players=new ArrayList<Player>(); //saranno due: il giocatore e Lorenzo
+    private final static int n_players =2;
+    private static DevelopeDecks developedecks[];
+    private static LeaderDeck leaderdeck;
+    private static Market market;
+    private static int VR_SG=0;
+    private static ActionStructure actionStructure = new ActionStructure();
+    //private static int timer_VR_SG=0;   non utile perchè Lorenzo non guadagna punti se si trova in area Vatican Report
 
-    public Lorenzo getLorenzo(){return Lore;}
+    public static ArrayList<Player> getPlayers() {
+        return players;
+    }
 
-    public static void  main (){
-        ActionStructure actionStructure = new ActionStructure();
+    public static void setPlayers(ArrayList<Player> players) {
+        SingleGame.players = players;
+    }
 
-        //mischio i segnalini appena comincia la partita
-        actionStructure.ShuffleSignal();
+    public static int getN_players() {
+        return n_players;
+    }
+
+    public static DevelopeDecks[] getDevelopedecks() {
+        return developedecks;
+    }
+
+    public static void setDevelopedecks(DevelopeDecks[] developedecks) {
+        SingleGame.developedecks = developedecks;
+    }
+
+    public static LeaderDeck getLeaderdeck() {
+        return leaderdeck;
+    }
+
+    public static void setLeaderdeck(LeaderDeck leaderdeck) {
+        SingleGame.leaderdeck = leaderdeck;
+    }
+
+    public static Market getMarket() {
+        return market;
+    }
+
+    public static void setMarket(Market market) {
+        SingleGame.market = market;
+    }
+
+    public static int getVR_SG() {
+        return VR_SG;
+    }
+
+    public static void setVR_SG(int VR_SG) {
+        SingleGame.VR_SG = VR_SG;
+    }
+
+    /*public static int getTimer_VR_SG() {
+        return timer_VR_SG;
+    }
+
+    public static void setTimer_VR_SG(int timer_VR_SG) {
+        SingleGame.timer_VR_SG = timer_VR_SG;
+    }
+    */
+
+    public static void  main () throws Exception {
+        SingleGame singleGame = new SingleGame();
+        Scanner scan = new Scanner(System.in);
+        //la modalità SingleGame è stata creata e attivata in Game-main quando il giocatore sceglie di giocare da solo
+        //si crea il nuovo e solo giocatore
+        Player you= new Player();
+        players.add(you);
+        you.setNumber(1);
+        //si chiede il nome/username
+        System.out.println("What's your name?");
+        you.setName(scan.nextLine());
+        //si aggiunge Lorenzo come secondo giocatore
+        Lorenzo lore = new Lorenzo();
+        Player LM = new Player();
+        players.add(LM);
+        LM.setName(lore.getName());
+        //Si inizializza tutto:
+        //- i segnalini azione (li mischio)
+        ActionStructure.ShuffleSignal();
+        //- i dodici mazzi di Development Cards
+        developedecks = new DevelopeDecks[12];
+        //- i LeaderDeck
+        leaderdeck = new LeaderDeck();
+
+        //creo l'oggetto turno
+        Turn turn = new Turn();
+
+        int actualplayer=0;
+
+
+        while(true) {
+            //ogni giocatore esegue il suo turno
+            turn.setActualplayer(players.get(actualplayer));
+            turn.main();
+
+
+            /**DA RIVEDERE E CORREGGERE: INSERIRE I CASI SOLO PER CUI O LORE VINCE O IL PLAYER VINCE**/
+            //4)
+            if (singleGame.Endgame(players.get(actualplayer))) {
+                System.out.println("The winner is " + singleGame.Victory());
+                return;
+            }
+
+            actualplayer++;
+            if(actualplayer>=players.size()) actualplayer=0;
+        }
+
+
+
+
+
+
+
     }
 
 
