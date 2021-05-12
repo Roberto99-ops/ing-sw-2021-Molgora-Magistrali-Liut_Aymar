36dfc8b7870c106f287f6752792375b0724e4e73
juan <juandavid.liut@mail.polimi.it>
2021-05-12 22:00:13 +0200

    - added newThread(): this one controls the comunication with one client at a time

--- src/main/java/it/polimi/ingsw/Server/Server.java
+++ src/main/java/it/polimi/ingsw/Server/Server.java
@@ -3,30 +3,36 @@ package it.polimi.ingsw.Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class Server {
+    private static Random random = new Random();
 
     //non deve essere costante la porta
-    public static int SOCKET_PORT = 1111;
+    //il numero di porta viene generato casualmente tra 0 e 7
+    public static int SOCKET_PORT = random.nextInt(8);
 
-    public void main (String[] agrs){
+    public static void main (){
         //apro la porta # ...
         ServerSocket socket;
         try {
+            //... creando un socket per quel numero di porta
             socket = new ServerSocket(SOCKET_PORT);
         } catch (IOException e) {
             System.out.println("cannot open server socket");
             System.exit(1);
             return;
         }
-        //...e il server si mette in ascolto. Per ongi nuova connessione stabilita, viene creato un nuovo thread di ClientHandler
+        //...e il server si mette in ascolto. Per ogni nuova connessione stabilita, viene creato un nuovo thread di ClientHandler
         while (true) {
             try {
                 /* accepts connections; for every connection we accept,
-                 * create a new Thread executing a ClientHandler */
+                 * create -- a new Thread executing a ClientHandler -- */
                 Socket client = socket.accept();
+                //il clientHandler si occupa di gestire la connessione con il client
                 ClientHandler clientHandler = new ClientHandler(client);
-                clientHandler.run();
+                //bisogna creare un nuovo thread che si occupi di gestire il clienthandler
+                Thread thread = new Thread(clientHandler, "server_" + client.getInetAddress());
             } catch (IOException e) {
                 System.out.println("connection dropped");
             }
