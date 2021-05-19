88f18a15b2805ba7bfed8aeab11474b23d4ff554
juan <juandavid.liut@mail.polimi.it>
2021-05-19 16:47:52 +0200

    - created ActionSignalMsg

---
+++ src/main/java/it/polimi/ingsw/Server/messages/ActionSignalMsg.java
@@ -0,0 +1,15 @@
+//package it.polimi.ingsw.Server.messages;
+
+/*OBSOLETO - LA NOTIFICA DEL SEGNALINO PRESO E DELLE SUE AZIONI PUO' ESSERE DATA DA ACTIONSTRUCTURE
+import it.polimi.ingsw.model.ActionSignal;
+
+public class ActionSignalMsg extends NetworkMessage {
+    private ActionSignal actionSignal;
+
+    public ActionSignalMsg(ActionSignal actionSignal){this.actionSignal=actionSignal;}
+
+    public ActionSignal getAction() {
+        return actionSignal;
+    }
+}
+ */
