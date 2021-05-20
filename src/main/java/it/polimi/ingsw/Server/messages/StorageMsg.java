447a76842dd050ea77b1e9bb321fc20634fd55a4
juan <juandavid.liut@mail.polimi.it>
2021-05-20 17:05:44 +0200

    - added JavaDoc

--- src/main/java/it/polimi/ingsw/Server/messages/StorageMsg.java
+++ src/main/java/it/polimi/ingsw/Server/messages/StorageMsg.java
@@ -15,7 +15,14 @@ public class StorageMsg extends NetworkMessage{
 
     //secondo me (Juan), il metodo getTotResource può essere sostituito semplicemente da una somma dei valori di ritorno
     //di countTypeS
+
+    /**
+     * It sends a message with all the available resources
+     * @return message as a string with all the available resources written
+     */
     public String available() {
         return "Available resources in your storage:\nRocks(G): "+storage.countTypeS('G')+"\nShields(B): "+storage.countTypeS('B')+"\nServants(P): "+storage.countTypeS('P')+"\nCoins(Y): "+storage.countTypeS('Y')+"\n";
     }
+
+
 }
