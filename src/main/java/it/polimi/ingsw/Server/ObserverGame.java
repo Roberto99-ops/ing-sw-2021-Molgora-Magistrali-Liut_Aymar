4ae56b8284912f4d7c429485f64e569294adbf74
fm <francesco.magistrali@mail.polimi.it>
2021-05-27 15:01:03 +0200

    Class' VirtualView deleted

--- src/main/java/it/polimi/ingsw/Server/ObserverGame.java
+++ src/main/java/it/polimi/ingsw/Server/ObserverGame.java
@@ -3,6 +3,23 @@ package it.polimi.ingsw.Server;
 import it.polimi.ingsw.Server.messages.MarketMessage;
 import it.polimi.ingsw.model.*;
 
+
+/*
+
+rappresenta il client dentro al server
+obeserver observable su model internet, comunica al controller le scelte dell'utente.
+una classe che osserva il model e che se la virtual view riceve le notifiche lo sa
+
+
+client - clienthendler - controller - model - virtualviewvedemodelmodificato -
+
+clienthendler grazie alla virtual view aggiorna l'utente usando pattern state per sveglire il tipo di messaggio
+da mandare (json o stringa o...) se si possono inviare file facciamo file json (oppure istanze di classi messaggio?)
+per aggiornamenti del tipo planciastringhe per messaggio "video" esempio su chi ha vinto
+
+*/
+
+
 public class ObserverGame extends Player {
 
     public static void updateMarket() {
