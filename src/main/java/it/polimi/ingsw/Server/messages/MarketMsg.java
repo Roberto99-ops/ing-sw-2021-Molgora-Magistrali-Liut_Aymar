c8f1d96e2f7662c956c34a59a15ad324a30c50ba
juan <juandavid.liut@mail.polimi.it>
2021-05-28 16:39:02 +0200

    - minor changes (changed name MarketMessage to MarketMsg)

--- src/main/java/it/polimi/ingsw/Server/messages/MarketMsg.java
+++ src/main/java/it/polimi/ingsw/Server/messages/MarketMsg.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.Server.messages;
 import it.polimi.ingsw.model.Market;
 import it.polimi.ingsw.model.Storage;
 
-public class MarketMessage extends NetworkMessage {
+public class MarketMsg extends NetworkMessage {
 
         private Market market;
 
@@ -11,7 +11,7 @@ public class MarketMessage extends NetworkMessage {
      * Prepares the data the Server is going to send to the Client about the actual situation of Market
      * @param mark the data requested
      */
-    public void MarketMsg (Market mark) {  this.market = mark; }
+    public MarketMsg (Market mark) {  this.market = mark; }
 
     /**
      * Sends the data of the Market
