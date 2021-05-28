58ca156e50ddda1fd502e2fa71f844688fd06641
juan <juandavid.liut@mail.polimi.it>
2021-05-28 16:33:10 +0200

    - DevelopeDeckMsg: Arraylists has been changed with DevelopeDeck - Setters and Getters have been added

--- src/main/java/it/polimi/ingsw/Server/messages/DevelopeDeckMsg.java
+++ src/main/java/it/polimi/ingsw/Server/messages/DevelopeDeckMsg.java
@@ -12,24 +12,38 @@ import java.util.ArrayList;
  */
 
 public class DevelopeDeckMsg extends NetworkMessage {
-
-    private ArrayList<Integer> array;
-
-    public DevelopeDeckMsg()
-    {
-        array = new ArrayList<>();
-    }
-
-    public ArrayList<Integer> getArray() {
-        return array;
-    }
+    private DevelopeDecks developeDecks = new DevelopeDecks();
+    //private ArrayList<Integer> array;
 
     /**
+     * Setter for the instance 'developeDecks': it will be the data sent to the client
+     * @param developeDecks
+     */
+    public  DevelopeDeckMsg(DevelopeDecks developeDecks){
+        this.developeDecks = developeDecks;
+    }
+
+    /*public void DevelopeDeckMsgAB()
+    {
+        array = new ArrayList<>();
+    }*/
+
+    /*
+    public ArrayList<Integer> getArray() {
+        return array;
+    }*/
+
+    /**
+     * Getter for the data we want the Server to send to the Client
+     * @return
+     */
+    public DevelopeDecks getDeck(){return developeDecks;}
+
+    /*
      * This method needs to be called by the receiver, to "unpack" the array of integer and
      * transform it into a deck of Developecards.
      * @return the deck of Developecards
      * @throws FileNotFoundException
-     */
 
     public DevelopeDecks getDeck() throws FileNotFoundException {
         DevelopeDecks deck = new DevelopeDecks();
@@ -40,4 +54,6 @@ public class DevelopeDeckMsg extends NetworkMessage {
         }
         return deck;
     }
+
+     */
 }
