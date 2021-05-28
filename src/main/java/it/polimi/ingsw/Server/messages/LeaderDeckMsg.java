ce410cdfef5d3ef95270ce3a5ff0947bc8e3d909
juan <juandavid.liut@mail.polimi.it>
2021-05-28 16:35:14 +0200

    - LeaderDeckMsg: Arraylist has been changed with LeaderDeck - Setters and Getters have been added

--- src/main/java/it/polimi/ingsw/Server/messages/LeaderDeckMsg.java
+++ src/main/java/it/polimi/ingsw/Server/messages/LeaderDeckMsg.java
@@ -10,23 +10,43 @@ import java.util.ArrayList;
  * Every element of the array is the position fo that card into the json file.
  */
 public class LeaderDeckMsg extends NetworkMessage{
-    private ArrayList<Integer> array;
+    private LeaderDeck leaderDeck= new LeaderDeck();
+    //private ArrayList<Integer> array;
 
-    public LeaderDeckMsg()
+    /**
+     * Setter for the instance 'leaderDeck': it will be the data sent to the client
+     * @param leaderDeck
+     */
+    public LeaderDeckMsg(LeaderDeck leaderDeck){
+        this.leaderDeck=leaderDeck;
+    }
+
+    /*public LeaderDeckMsg()
     {
         array = new ArrayList<>();
     }
+     */
 
+
+    /*
     public ArrayList<Integer> getArray() {
         return array;
     }
 
+     */
+
     /**
+     * Getter for the data we want the Server to send to the Client
+     * @return
+     */
+    public LeaderDeck getDeck(){return leaderDeck;}
+
+    /*
      * This method has to be called by the receiver, to "unpack" the array of integer and
      * trasform it into a deck of leadercard.
      * @return the deck of leadercard
      * @throws FileNotFoundException
-     */
+
     public LeaderDeck getDeck() throws FileNotFoundException {
         LeaderDeck deck = new LeaderDeck();
         for (int i = 0; i < this.getArray().size(); i++) {
@@ -36,4 +56,6 @@ public class LeaderDeckMsg extends NetworkMessage{
         }
         return deck;
     }
+
+     */
 }
