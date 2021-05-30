f6dd33fe12582cf14910467aecd72f8b71e155af
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-05-31 01:05:24 +0200

    now climanager updates the player all in the same if, and prints whenever it's called

--- src/main/java/it/polimi/ingsw/view/cli/CliManager.java
+++ src/main/java/it/polimi/ingsw/view/cli/CliManager.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.view.cli;
 import it.polimi.ingsw.Server.messages.*;
 import it.polimi.ingsw.model.*;
 
+import java.io.FileNotFoundException;
+import java.lang.ref.Cleaner;
+
 /**
  * this class recognize what type of message the client received;
  * then update the view.
@@ -14,26 +17,44 @@ public class CliManager {
     private static LeaderDeck LDeck;
     private static ActionSignal signal;
 
-    public static void Update(Object msg, Player player)
-    {
+    public static void Update(Object msg, Player player) throws FileNotFoundException {
         if(msg.getClass().equals(MarketMsg.class)) {
             market = new Market();
             MarketMsg marketMsg = (MarketMsg)msg;
             market.setMatrix(marketMsg.getMarket().getMatrix());
             market.setExtraball(marketMsg.getMarket().getExtraball());
+            MarketView marketView = new MarketView(market);
+            marketView.Print();
         }
 
-        if(msg.getClass().equals(StorageMsg.class)) {
+        /*if(msg.getClass().equals(StorageMsg.class)) {
             StorageMsg storageMsg = (StorageMsg) msg;
             player.setStorage(storageMsg.getStorage());
         }
         if(msg.getClass().equals(FaithTrackMsg.class)) {
             FaithTrackMsg faithTrackMsg = (FaithTrackMsg) msg;
-           // player.setTrackposition();   implementare meglio faithtrackmsg, deve inviare un int con dentro trackposition
+            // player.setTrackposition();   implementare meglio faithtrackmsg, deve inviare un int con dentro trackposition
         }
-        if(msg.getClass().equals(LeaderDeck.class)) {
+        if(msg.getClass().equals(LeaderDeckMsg.class)) {
             LeaderDeckMsg leaderDeckMsg = (LeaderDeckMsg) msg;
             player.setLeadercards(leaderDeckMsg.getDeck());
+        }*/
+        if(msg.getClass().equals(PlayerMsg.class)) {
+            PlayerMsg playerMsg = (PlayerMsg) msg;
+            player.setLeadercards(playerMsg.getPlayer().getLeadercards());
+            player.setTrackposition(playerMsg.getPlayer().getTrackposition());
+            player.setStorage(playerMsg.getPlayer().getStorage());
+            player.setName(playerMsg.getPlayer().getName());
+            player.setDSpace(playerMsg.getPlayer().getDSpace());
+            player.setDevelopementquantity(playerMsg.getPlayer().getDevelopementquantity());
+            player.setMinideck1(playerMsg.getPlayer().getMinideck1());
+            player.setMinideck2(playerMsg.getPlayer().getMinideck2());
+            player.setMinideck3(playerMsg.getPlayer().getMinideck3());
+            player.setNumber(playerMsg.getPlayer().getNumber());
+            player.setPv(playerMsg.getPlayer().getPv());
+
+            Playerboard board = new Playerboard(player);
+            board.Print();
         }
         //...
 
