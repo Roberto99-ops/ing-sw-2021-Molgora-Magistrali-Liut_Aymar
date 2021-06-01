baee178df1683e8d302165537d59c8f7ce00413b
Roberto99-ops <roberto.molgora@mail.polimi.it>
2021-06-01 11:33:23 +0200

    singlegameManager implemented

--- src/main/java/it/polimi/ingsw/controller/SingleGameManager.java
+++ src/main/java/it/polimi/ingsw/controller/SingleGameManager.java
@@ -5,17 +5,19 @@ import it.polimi.ingsw.Server.messages.PlayerMsg;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.SingleGame;
+import it.polimi.ingsw.model.Turn;
 
 import java.io.IOException;
 
 /**
  * it manages a game in singleplayer. it doesn't extend GameManger because
- * it have would simply overrided the main method that is static and that's not possible.
+ * it would have simply overrided the main method that is static and that's not possible.
  */
 public class SingleGameManager{
 
     private static SingleGame game;
     private static ClientHandler client;
+    private static Player L;
 
     /**
      * costructor. the clienthandler istance is passed so is easy to
@@ -27,11 +29,29 @@ public class SingleGameManager{
         client = clientin;
     }
 
-    public static void main() throws IOException {
+    public static void main() throws Exception {
         Player player = client.getPlayer();
         PlayerMsg msg = new PlayerMsg(player);
         client.sendMessage(msg);
         game.getPlayers().add(player);
-        client.sendMessage("Turn Finished");
+        //mischiare segnalini e carte e market
+        Shuffle();
+
+        while(!game.Endgame(player)) {
+            TurnManager turnManager = new TurnManager();
+            //Turn turn = new Turn();
+            //turn.setActualplayer(player);
+            turnManager.main(client, game, 0);
+            client.sendMessage("Turn Finished");
+        }
+
+        String winner = game.Victory();
+        client.sendMessage(winner);
+    }
+
+    private static void Shuffle()
+    {
+        SingleGame.getActionStructure().ShuffleSignal();
+        //mischiare mazzi e mercato
     }
 }
