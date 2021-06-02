3b158595bba4f526ca73f555de67c8682646f6f0
fm <francesco.magistrali@mail.polimi.it>
2021-06-02 16:10:41 +0200

    classes updateationstructure

--- src/main/java/it/polimi/ingsw/Server/ObserverSingleGame.java
+++ src/main/java/it/polimi/ingsw/Server/ObserverSingleGame.java
@@ -1,14 +1,16 @@
 package it.polimi.ingsw.Server;
 
+import it.polimi.ingsw.Server.messages.ActionStructureMsg;
+import it.polimi.ingsw.Server.messages.MarketMsg;
 import it.polimi.ingsw.model.ActionStructure;
 import it.polimi.ingsw.model.SingleGame;
 
 public class ObserverSingleGame extends ObserverGame {
 
-    public static void updateActionStructure() {
+    public void updateActionStructure(ClientHandler clh) throws Exception {
         ActionStructure actualActionStructure = SingleGame.getActionStructure();
-        System.out.println("actionstructuremessage");
-        // ObservableGame.notifyAllObservers();
+        ActionStructureMsg msg = new ActionStructureMsg(actualActionStructure);
+        clh.sendMessage(msg);
     }
 
 }
