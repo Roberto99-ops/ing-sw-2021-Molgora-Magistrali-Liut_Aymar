2c7fe13038c110651ea335979d9e1bff64b7ed28
fm <francesco.magistrali@mail.polimi.it>
2021-06-06 10:59:54 +0200

    GameManager small changes

--- src/main/java/it/polimi/ingsw/controller/GameManager.java
+++ src/main/java/it/polimi/ingsw/controller/GameManager.java
@@ -122,7 +122,7 @@ public class GameManager {
         while (true) {
             //3)
             //turnmanager.setActualplayer(game.getPlayers().get(actualplayer));
-            turnmanager.main(clientList.get(actualplayer), game, actualplayer, obsG);
+            turnmanager.main(clientList.get(actualplayer), game, actualplayer);
 
             //4)
             if (game.callEndgame(game.getPlayers().get(actualplayer))) {
