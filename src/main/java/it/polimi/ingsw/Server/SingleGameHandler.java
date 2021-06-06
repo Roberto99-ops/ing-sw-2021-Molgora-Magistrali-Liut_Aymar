4eefe74f0c2fae4da59c816a044b3bbb918bb1e8
fm <francesco.magistrali@mail.polimi.it>
2021-06-06 13:49:20 +0200

    Rename of this class

--- src/main/java/it/polimi/ingsw/Server/SingleGameHandler.java
+++ src/main/java/it/polimi/ingsw/Server/SingleGameHandler.java
@@ -1,11 +1,10 @@
 package it.polimi.ingsw.Server;
 
 import it.polimi.ingsw.Server.messages.ActionStructureMsg;
-import it.polimi.ingsw.Server.messages.MarketMsg;
 import it.polimi.ingsw.model.ActionStructure;
 import it.polimi.ingsw.model.SingleGame;
 
-public class ObserverSingleGame extends ObserverGame {
+public class SingleGameHandler extends GameHandler {
 
     public void updateActionStructure(ClientHandler clh) throws Exception {
         ActionStructure actualActionStructure = SingleGame.getActionStructure();
