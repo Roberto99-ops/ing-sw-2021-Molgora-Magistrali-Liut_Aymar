8b5f11d77d3f48a7c5cb79e4c028ce2cc3101381
fm <francesco.magistrali@mail.polimi.it>
2021-06-06 10:59:00 +0200

    ObservableSingleGame class created

---
+++ src/main/java/it/polimi/ingsw/Server/ObservableSingleGame.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.Server;
+
+import java.util.Observer;
+
+public class ObservableSingleGame {
+
+    private ClientHandler clh;
+
+    private ObserverSingleGame observerSingleGame;
+
+    public  ObserverSingleGame getObserverSingleGame() {
+        return observerSingleGame;
+    }
+
+    public void setObserverSingleGame(ObserverSingleGame observerSingleGame) {
+        this.observerSingleGame = observerSingleGame;
+    }
+
+
+    public void notifySingleGameObserver () throws Exception {
+
+        observerSingleGame.updateDevelopementDecks(clh);
+        observerSingleGame.updateLeaderDeck(clh);
+        observerSingleGame.updateMarket(clh);
+        observerSingleGame.updateTimer_VR(clh);
+        observerSingleGame.updateVR(clh);
+        observerSingleGame.updateDevelopementSpace(clh);
+        observerSingleGame.updateStorage(clh);
+        observerSingleGame.updateStrongbox(clh);
+        observerSingleGame.updateLeaderCards(clh);
+        observerSingleGame.updateDevelopementQuantity(clh);
+        observerSingleGame.updateSkill1(clh);
+        observerSingleGame.updateSkill2(clh);
+        observerSingleGame.updatePV(clh);
+        observerSingleGame.updateFaithTrack(clh);
+
+    }
+
+}
+
+
+
