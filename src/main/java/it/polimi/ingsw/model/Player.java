498b50d76c246d64164ab33cf01975636ae9628b
fm <francesco.magistrali@mail.polimi.it>
2021-06-09 20:03:16 +0200

    Player method addresource in storage debugged

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -354,60 +354,96 @@ public class Player implements Serializable {
      * @param resource : the resource the player will put in Storage
      */
     public boolean addResourceStorage(char resource) {
-        /*IDEA: scelgo una risorsa e questa , che si trova dentro il resourcestructure, va inserita dentro il magazzino
+        /*IDEA: scelgo una risorsa e questa , che si trova dentro il resourcestructure,
+        va inserita dentro il magazzino
         (se viene dal mercato)
         */
         //caso in cui il magazzino sia pieno (sia extrapanel sia panel)
-        if (storage.countTypeS('N')==0) {
+
+        if (storage.countTypeS('N') == 0) {
             System.out.println("No more space available in Storage.");
             return false;
         }
 
+
         //caso in cui il magazzino abbia uno o + spazi vuoti
 
-        int i = 5;
+
+        //controllo se extrapanel sia dello stesso tipo e se ha degli spazi liberi
         int countExtraN = 0;
         for (int c = 1; c >= 0; c--) {
             if (storage.getExtrapanel().getVector().get(c).equals('N')) countExtraN++;
         }
-        //controllo se extrapanel sia dello stesso tipo e se ha degli spazi liberi
+
         if (storage.getTypeExtrapanel() == resource && countExtraN > 0) {
             if (storage.getExtrapanel().getVector().get(0) == 'N')
                 storage.getExtrapanel().getVector().set(0, resource);
-             else if (storage.getExtrapanel().getVector().get(1) == 'N')
+            else if (storage.getExtrapanel().getVector().get(1) == 'N')
                 storage.getExtrapanel().getVector().set(1, resource);
 
-             return true;
-        } //else if (storage.getTypeExtrapanel()!=resource && countExtraN== storage.countTypeS(resource))
-        //System.out.println("No more space available in Storage.");
+            return true;
+
+        } else if (storage.getTypeExtrapanel() != resource && countExtraN == storage.countTypeS(resource))
+            System.out.println("No more space available in Storage");
 
         //controllo panel:
+
+
         //-se c'è già una presente vedo se aggiungerne un'altra
+
+        int i = 5;
+
         if (storage.getPanel().contains(resource)) {
             while (storage.getPanel().get(i) != resource && i >= 0) {
                 i--;
             }
-            if (i == 0 || i == 2 || i == 5) {
-                // i==0 : una risorsa di quel tipo presente in cima -> elimino la risorsa
-                // i==2 : il secondo piano ha risorse di quel tipo -> elimino la risorsa
-                // i==5 : il terzo piano ha risorse di quel tipo -> elimino la risorsa
-                System.out.println(resource + " deleted. It already exists");
+
+            if (i == 0 || i == 2) {
+
+                // i==0 : provo a vedere se riesco a sostituire i piani
+
+                if (storage.switchresources(resource, 0) == true) {
+                    System.out.println(resource + " switched using best case");
+                    return true;
+                } else {
+                    System.out.println(resource + " can't be added because of is present in panel and panels can't be switched ");
+                }
+
+                // i==2 : provo a vedere se riesco a sostituire i piani
+
+                if (storage.switchresources(resource, 2) == true) {
+                    System.out.println(resource + " switched using best case");
+                    return true;
+                } else System.out.println(resource + " can't be added because of is present in panel and panels can't be switched ");
+
+
             }
+
+            // i==5 : il terzo piano ha risorse di quel tipo -> elimino la risorsa
+            if (i == 5) {
+                System.out.println(resource + " deleted. It already exists");
+                return false;
+            }
+
             // in tutti gli altri casi in cui un posto nei piani è libero
             switch (i) {
                 case 1:
                     storage.getPanel().set(2, resource); //posto i==2 ora occupato
-                break;
+                    break;
                 case 3:
                     storage.getPanel().set(4, resource); //posto i==4 ora occupato
-                break;
+                    break;
                 case 4:
                     storage.getPanel().set(5, resource); //posto i==5 ora occupato
-                break;
+                    break;
             }
+
             return true;
+
         } else {
-            //se la risorsa non c'è:
+
+            //se la risorsa non c'è
+
             if (storage.getPanel().get(0) == 'N') {
                 i = 0;
             } else if ((storage.getPanel().get(1) == 'N') && (storage.getPanel().get(2) == 'N')) {
@@ -415,14 +451,16 @@ public class Player implements Serializable {
             } else if ((storage.getPanel().get(3) == 'N') && (storage.getPanel().get(4) == 'N') && (storage.getPanel().get(5) == 'N')) {
                 i = 3;
             }
-            if(i < 4) //this if is here to avoid the case which we have a fourth type of resource, so is not contained in the storage yet, but we don't have to put it inside
-                storage.getPanel().set(i, resource); //aggiungo la risorsa nel primo piano (i==0) o nel secondo (i==1) o nel terzo (i==3)
-            else
-                System.out.println(resource + " deleted. It can't be put in the storage");
-            return true;
-        }
-        //conto il numero di N in extrapanel
 
+            if (i < 4) {
+                //this if is here to avoid the case which we have a fourth type of resource, so is not contained in the storage yet, but we don't have to put it inside
+                storage.getPanel().set(i, resource); //aggiungo la risorsa nel primo piano (i==0) o nel secondo (i==1) o nel terzo (i==3)
+                return true;
+            } else {
+                System.out.println(resource + " deleted. It can't be put in the storage");
+                return false;
+            }
+        }
     }
 
     /**
