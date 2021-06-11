c517f7b7c717e8b3ae62fee165de420824cc418e
fm <francesco.magistrali@mail.polimi.it>
2021-06-11 11:05:35 +0200

    Deck Checked, Sort and Cleaned

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -3,53 +3,71 @@ import java.io.Serializable;
 import java.util.Random;
 import java.util.ArrayList;
 
+
+
 /**
  * abstract class with generics so we can override from developedeck and leaderdeck
  * @param <T>
  */
+
 public abstract class Deck <T> implements Serializable {
     private ArrayList<T> structure;
 
-    //restituisce il mazzo
+
+
+    /**
+     getter and setter
+     */
+
     public ArrayList<T> getStructure() {
         return structure;
     }
-
-    //in teoria non serve
     public void setStructure(ArrayList<T> structure) {
         this.structure = structure;
     }
+
+
+
 
     /**
      * method that allow us to pick the card on the top of the deck
      * @param structure
      * @return
      */
-    public T pick(ArrayList<T> structure)
-    {
+
+    public T pick(ArrayList<T> structure) {
+
         T firstElement;
         firstElement=structure.get(0);
         structure.remove(0);
         return firstElement;
     }
 
-    //chiedere se meglio così o con funzione nativa
+
+
+
     /**
      * method that randomly mix the deck
      * @param structure
      * @return
      */
-    public ArrayList<T> shuffleDeck(ArrayList<T> structure)
-    {
+
+    public ArrayList<T> shuffleDeck(ArrayList<T> structure) {
+
         ArrayList<T> mixedStructure = new ArrayList<T>();
         Random mixer = new Random();
-        while(structure.size() > 0)
-        {
+
+        while(structure.size() > 0) {
             int mix = mixer.nextInt(structure.size());
             mixedStructure.add(structure.get(mix));
             structure.remove(mix);
         }
+
         return mixedStructure;
     }
+
+
+
+
 
 }
