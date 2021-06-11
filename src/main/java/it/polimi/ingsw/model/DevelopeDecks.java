19e32242b69a07ea66d25a3a3127a3b69eff5d61
fm <francesco.magistrali@mail.polimi.it>
2021-06-11 11:24:58 +0200

    DevelopeDecks Checked, Sort and Cleaned

--- src/main/java/it/polimi/ingsw/model/DevelopeDecks.java
+++ src/main/java/it/polimi/ingsw/model/DevelopeDecks.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+
 public class DevelopeDecks extends Deck<DevelopeCard> implements Serializable {
     private ArrayList<DevelopeCard> structure = new ArrayList<DevelopeCard>();
 
@@ -10,19 +11,26 @@ public class DevelopeDecks extends Deck<DevelopeCard> implements Serializable {
      * to modify decks there are methods (generics) in Deck class
      * @return
      */
+
     @Override
     public ArrayList<DevelopeCard> getStructure() {
         return structure;
     }
-
-
     @Override
     public void setStructure(ArrayList<DevelopeCard> structure) {
         this.structure = structure;
     }
 
+
+
+    /**
+     * print the deck using generic method Print in Deck class
+     */
     public void print()
     {
         for(int i=0; i<this.structure.size(); i++) this.structure.get(i).print();
     }
+
+
+
 }
