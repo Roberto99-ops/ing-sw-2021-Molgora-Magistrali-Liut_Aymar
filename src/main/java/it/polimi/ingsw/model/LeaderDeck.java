26fdf7ae6f765ec16aac656acfad9357d6a29ce8
fm <francesco.magistrali@mail.polimi.it>
2021-06-11 16:08:10 +0200

    Leaderdeck, Sort and Cleaned

--- src/main/java/it/polimi/ingsw/model/LeaderDeck.java
+++ src/main/java/it/polimi/ingsw/model/LeaderDeck.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class LeaderDeck extends Deck <LeaderCard> implements Serializable {
+
     private ArrayList<LeaderCard> structure = new ArrayList<LeaderCard>();
 
     /**
