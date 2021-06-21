ab5bf55ebe27a323872797868f033623d7cb097e
fm <francesco.magistrali@mail.polimi.it>
2021-06-21 11:43:24 +0200

    ResourceStructure labor limae: methods unused deleted, comments review

--- src/main/java/it/polimi/ingsw/model/ResourceStructure.java
+++ src/main/java/it/polimi/ingsw/model/ResourceStructure.java
@@ -1,71 +1,53 @@
 package it.polimi.ingsw.model;
 import java.util.ArrayList;
 public class ResourceStructure extends ArrayList {
+
+
     /**
-     * resourcestructure is an arraylist of char which can be W,R,B,G,P,Y
+     * Resourcestructure is an arraylist of char which can be W,R,B,G,P,Y
      */
+
     private ArrayList<Character> vector;
 
-    public ResourceStructure()
-    {
+
+    /**
+     * costruction of Resourcestructure
+     */
+
+    public ResourceStructure() {
         vector = new ArrayList<>();
     }
 
-    public ArrayList<Character> getVector() { return vector; }
 
+
+    /**
+     * getter and setter
+     * * @return vectore of Resourcestructure (getter)
+     */
+
+    public ArrayList<Character> getVector() { return vector; }
     public void setVector(ArrayList<Character> vector) { this.vector = vector; }
 
-    /**
-     * this method reorder the resourcestructure, for type of resource, in this order: Y,G,P,B,W,R
-     * @param vector
-     * @return
-     */
-    public ArrayList<Character> Order(ArrayList<Character> vector)
-    {
-        // da implementare ?
-        return vector;
-    }
 
-
-    /**
-     * this method checks if the given structure is composed by only one type of resource
-     * @param vector
-     * @return
-     */
-    public boolean checkEquality(ArrayList<Character> vector) {
-        int counter = 0;
-        if(vector.size()==0) return true;
-        Character first = vector.get(0);
-        for(int i=0; i < vector.size(); i++) {
-            if (vector.get(i) == first) {
-                counter ++;
-            }
-        }
-    if (counter == vector.size()) {
-        return true;
-    } else {
-        return false;
-    }
-
-    }
 
     /**
      * this method adds a given quantity of a resource type to the caller structure
      * @param quantity
      * @param resource
-     * @return
+     * @return the structure built with the parameters in input
      */
-    public ArrayList<Character> addResource(int quantity, Character resource) {//differenza tipi primitivi e normali
+
+    public ArrayList<Character> addResource(int quantity, Character resource) {
         for(int i=0; i<quantity; i++) vector.add(resource);
         return(vector);
     }
 
 
     /**
-     * return the position of the first element of a specific type of resource
      * @param resource
-     * @return
+     * @return the position of the first element of a specific type of resource
      */
+
     public int firstOccurance(char resource)
     {
         int i=0;
@@ -73,10 +55,12 @@ public class ResourceStructure extends ArrayList {
         return i;
     }
 
+
     /**
      * remove the first resource of a specific type
      * @param resource: resource to delete
      */
+
     public void removeThis(char resource)
     {
         int i=0;
