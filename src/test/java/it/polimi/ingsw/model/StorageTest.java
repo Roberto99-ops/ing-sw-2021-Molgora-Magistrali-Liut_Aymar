f33a03bd946b1155c8a2a368fa688007fa2b8251
fm <francesco.magistrali@mail.polimi.it>
2021-06-21 13:53:50 +0200

    StorageTest final implementation

--- src/test/java/it/polimi/ingsw/model/StorageTest.java
+++ src/test/java/it/polimi/ingsw/model/StorageTest.java
@@ -3,46 +3,59 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.Server.GameHandler;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class StorageTest {
 
     @Test
 
-    public void checkStorage() {
+    public void switchResourceTest() {
 
-
-        GameHandler observertested = new GameHandler();
-
+        Player player = new Player();
         Storage box = new Storage();
-        Storage scat = new Storage();
+        ArrayList <Character> obj = new ArrayList<>();
+
+        box.getPanel().set(0,'B');
+        box.getPanel().set(1,'P');
+        box.getPanel().set(3,'Y');
 
 
-        box.setinStorage('B',0);
-        box.setinStorage('P',1);
-        box.setinStorage('P',2);
-        box.setinStorage('W',3);
-        box.setinStorage('W',4);
-        box.setinStorage('W',5);
-        box.deleteResources(1,'W');
+        for(int i = 0; i < 6; i++) {
+            obj.add('N');
+        }
+
+
+        obj.set(0,'B');
+        obj.set(1,'P');
+        obj.set(3,'Y');
         box.printPanel();
+        assertEquals(obj, box.getPanel());
 
-        scat.setinStorage('B',0);
-        scat.setinStorage('P',1);
-        scat.setinStorage('P',2);
-        scat.setinStorage('P',3);
-        scat.setinStorage('W',4);
-        scat.setinStorage('W',5);
-        scat.printPanel();
+        box.switchresources('B',0);
+        box.printPanel();
+        obj.set(0, 'P');
+        obj.set(1, 'B');
+        obj.set(2, 'B');
+        assertEquals(obj, box.getPanel());
 
-        assertEquals(5, box.getTotResourceStorage());
-        assertEquals(6, scat.getTotResourceStorage());
-        assertEquals(1, box.countTypeS('B'));
-        assertEquals(2, box.countTypeS('P'));
-        assertEquals(true, box.checkStorage());
-        assertEquals(false, scat.checkStorage());
+        box.switchresources('B',2);
+        box.printPanel();
+        obj.set(1, 'Y');
+        obj.set(2, 'N');
+        obj.set(3, 'B');
+        obj.set(4, 'B');
+        obj.set(5, 'B');
+        assertEquals(obj, box.getPanel());
 
 
+        box.switchresources('P',0);
+        box.printPanel();
+        obj.set(0, 'Y');
+        obj.set(1, 'P');
+        obj.set(2, 'P');
+        assertEquals(obj, box.getPanel());
 
     }
 
