22c949d6e76ebe50b86623add4c086b6f7fa7c2e
fm <francesco.magistrali@mail.polimi.it>
2021-06-22 22:56:13 +0200

    DevelopementSpaceTest final implementation

--- src/test/java/it/polimi/ingsw/model/DevelopementSpaceTest.java
+++ src/test/java/it/polimi/ingsw/model/DevelopementSpaceTest.java
@@ -8,40 +8,50 @@ public class DevelopementSpaceTest extends TestCase {
 
     public void testGetCard() throws Exception {
         DevelopementSpace space = new DevelopementSpace();
+
         for (int i = 0; i < 2; i++) {
             DevelopeCard card = new DevelopeCard();
             card.setCard(i);
             space.getMinideck1().getStructure().add(card);
+            for (int k = 0; k < space.getMinideck1().getStructure().size() ; k++) {
+                space.getMinideck1().getStructure().get(k).print();
+            }
         }
 
         for (int i = 3; i < 5; i++) {
-            DevelopeCard card = new DevelopeCard();
-            card.setCard(i);
-            space.getMinideck2().getStructure().add(card);
+            DevelopeCard card1 = new DevelopeCard();
+            card1.setCard(i);
+            space.getMinideck2().getStructure().add(card1);
+            for (int k = 0; k < space.getMinideck2().getStructure().size() ; k++) {
+                space.getMinideck2().getStructure().get(k).print();
+            }
         }
 
         for (int i = 15; i < 17; i++) {
-            DevelopeCard card = new DevelopeCard();
-            card.setCard(i);
-            space.getMinideck3().getStructure().add(card.setCard(i));
+            DevelopeCard card2 = new DevelopeCard();
+            card2.setCard(i);
+            space.getMinideck3().getStructure().add(card2.setCard(i));
+            for (int k = 0; k < space.getMinideck3().getStructure().size() ; k++) {
+                space.getMinideck3().getStructure().get(k).print();
+            }
         }
 
         DevelopeCard card = space.getCard(1);
         assertEquals(1, card.getLevel());
         assertEquals('P', card.getColour());
         assertEquals(1, card.getPv());
-/*
+
         DevelopeCard card1 = space.getCard(2);
-        assertEquals(1, card.getLevel());
-        assertEquals('G', card.getColour());
-        assertEquals(2, card.getPv());
+        assertEquals(1, card1.getLevel());
+        assertEquals('G', card1.getColour());
+        assertEquals(2, card1.getPv());
 
         DevelopeCard card2 = space.getCard(3);
-        assertEquals(2, card.getLevel());
-        assertEquals('G', card.getColour());
-        assertEquals(5, card.getPv());
+        assertEquals(2, card2.getLevel());
+        assertEquals('G', card2.getColour());
+        assertEquals(5, card2.getPv());
 
- */
+
     }
 
 
