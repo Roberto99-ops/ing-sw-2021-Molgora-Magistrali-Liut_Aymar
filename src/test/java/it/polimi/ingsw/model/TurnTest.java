9fbe06f5b4e802a48c08aef53b6fa43263658b38
fm <francesco.magistrali@mail.polimi.it>
2021-06-27 11:25:24 +0200

    Turn test started

---
+++ src/test/java/it/polimi/ingsw/model/TurnTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+public class TurnTest {
+
+    @Test
+
+    public void shopcardTest() throws FileNotFoundException {
+
+
+    }
+
+    @Test
+
+    public void buyResourceTest() throws FileNotFoundException {
+
+
+    }
+
+
+
+    @Test
+
+    public void activateLeaderTest() throws FileNotFoundException {
+
+
+    }
+
+
+
+    @Test
+
+    public void removeLeaderTest() throws FileNotFoundException {
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
