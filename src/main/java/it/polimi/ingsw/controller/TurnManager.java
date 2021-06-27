7aac12b673a35fcf5712d729396048fc20c647c2
fm <francesco.magistrali@mail.polimi.it>
2021-06-27 11:24:02 +0200

    Class' coding cleaned

--- src/main/java/it/polimi/ingsw/controller/TurnManager.java
+++ src/main/java/it/polimi/ingsw/controller/TurnManager.java
@@ -20,18 +20,16 @@ public class TurnManager {
      * then the turn ends
      */
 
-    //actualplayer è la posizione del gocatore attuale nell'array dei giocatori di game
+
     public void main(ClientHandler client, Game game, int actualplayer) throws Exception {
         Turn turn = new Turn(client, game);
-        //turn.setActualplayer(game.getPlayers().get(actualplayer));
         GameHandler player = turn.getActualplayer();
         String reception;
-        //int action = 0;
         int leaderaction = 0;
         boolean additionalAction = false;
         boolean actionDone = true;
 
-        //se sono in single game, ogni volta che tocca a me, prendo un segnalino ed eseguo la sua azione
+
         if (game.getClass().equals(SingleGame.class)) {
             SingleGame.getActionStructure().pickSignal();
             System.out.println(SingleGame.getActionStructure().getAS_Counter() + " \t" + SingleGame.getActionStructure().getOLD_Signal() +  " " + Arrays.toString(SingleGame.getActionStructure().getStructure()));
@@ -43,61 +41,25 @@ public class TurnManager {
 
         String c = new String();
         do {
-            /*//A)
-            client.sendMessage("Do you want to do a Leader action? (yes/no)\n ");
-            reception = client.receiveMessage();
-            if (reception.equals("yes")){
-                client.sendMessage("Do you want to remove a card or activate one? (remove/activate) ");
-                String choice = client.receiveMessage();
 
-                if(choice.equals("remove")) {
-                    turn.removeLeader();
-                    leaderaction++;
-                }
-
-                if(choice.equals("activate")) {
-                    turn.activateLeader();
-                    leaderaction++;
-                }
-            }
-*/
-            //1)
             if (!actionDone){
                 client.sendMessage("Since you have done no actions:\nWhat do you want to do?\n\t1)Shop a developement card\t2)Take resources at the market\n\t3)Active a production\n");
             } else if (actionDone) {
                 client.sendMessage("What do you want to do?\n\t1)Shop a developement card\t2)Take resources at the market\n\t3)Active a production\n");//4)Do a Leader action ");
             }
                 String msg = client.receiveMessage();
-            //if(!msg.equals(""))
-              //  msg.charAt(0) - 48 = msg.charAt(0) - 48;
+
 
             try {
                 System.out.println(msg.charAt(0) - 48);
                 if (msg.charAt(0) - 48 == 1) {
                     if (!turn.shopCard()) {actionDone = false;}
-                    /*while (!turn.shopCard()) {
-                        do {
-                            client.sendMessage("Do you want to try buying another Develop Card (1) or doing another kind of action (2)?\n");
-                            msg = client.receiveMessage();
-                            action = msg.charAt(0) - 48;
-                        } while (action !=1 && action !=2);
-                        if (action == 2) {
-                            client.sendMessage("What do you want to do?\n\t2)Take resources at the market\n\t3)Active a production\t4)Do a Leader action ");
-                            String msg = client.receiveMessage();
-                            if(!msg.equals(""))
-                                action = msg.charAt(0) - 48;
-                        }
-
-                     */
                     else {
                         client.sendMessage("clean screen");
                         player.updatePlayerBoard(client, game);
                     }
                 }
 
-                // CONTROLLER:
-                // IN) IL NUMERO DI CARTE DA COMPRARE
-                // OUT) RIMUOVE LE RISORSE DI COSTO CARTA DALLA PLANCIA e AGGIUNGE NELLE CARTE SVILUPPO DI PLAYER LE CARTE VOLUTE
 
 
                 else if (msg.charAt(0) - 48 == 2) {
@@ -111,13 +73,9 @@ public class TurnManager {
                     actionDone = true;
                 }
 
-                // CONTROLLER:
-                // IN) N^ COLONNA/RIGA DEL MERCATO
-                // OUT) CAMBIA IL MERCATO e RITORNA LE RISORSE COMPRATE
-
 
                 else if (msg.charAt(0) - 48 == 3) {
-                     //2.1)
+
 
                     boolean ableTo;
 
@@ -147,25 +105,13 @@ public class TurnManager {
                                                 }
                                             }
 
-/*
-                            String cardChosen = client.receiveMessage();
-                            if (cardChosen.equals("0") || cardChosen.equals("1")) {
-                                int card = cardChosen.charAt(0) - 48;  //converts a char into the correspondant int
-                                if (card == 0 && player.getSkill1() == 1 && player.getLeadercards().getStructure().get(0).getSkill().equals("Prod.Skill"))
-                                    player.getLeadercards().getStructure().get(0).doProductionSkill(client, game);
-                                if (card == 1 && player.getSkill2() == 1 && player.getLeadercards().getStructure().get(1).getSkill().equals("Prod.Skill"))
-                                    player.getLeadercards().getStructure().get(1).doProductionSkill(client, game);
-                            }
 
- */
+                    ableTo = false;
 
-                    ableTo = false; //flag per fare produzione
-                        //2.2)
 
-                        boolean[] checks = new boolean[4]; //flag per le 4 operazioni
+                        boolean[] checks = new boolean[4];
                         char chosenResource = 'N';
-                        for (int i = 0; i < 4; i ++) checks[i] = false; //setto tutti e 4 i flag a false
-
+                        for (int i = 0; i < 4; i ++) checks[i] = false;
                         client.sendMessage("Do you want to activate the basic production? (y/n)\n");
                         String s=client.receiveMessage();
                         if (s.equals("y")) {
@@ -224,15 +170,6 @@ public class TurnManager {
                             }
                         }
 
-                        /*for(int i=0; i<vectorInProduction.size(); i++){
-                            System.out.println(vectorInProduction.get(i));
-                        }
-
-                         */
-                        //System.out.println(vectorInProduction.get(0)+"  "+vectorOutProduction.get(0));
-
-
-                       // System.out.println(turn.getActualplayer().checkResources(vectorInProduction)); //
 
                         if (turn.getActualplayer().checkResources(vectorInProduction) != 0) { //
                             client.sendMessage("You are able to do this production (press a key)\n");
@@ -265,15 +202,15 @@ public class TurnManager {
 
                 }
 
-                //in case we have done only 0 or 1 leaderactions during our turn, we can do another one at the end of the turn
+
                 if((leaderaction<2 && (msg.charAt(0) - 48 == 1 || msg.charAt(0) - 48 == 2 || msg.charAt(0) - 48 == 3)) && player.getLeadercards().getStructure().size()>0) {
                     client.sendMessage("Do you want to do a Leader action? (yes/no)\n ");
                     reception = client.receiveMessage();
                     if(reception.equals("yes")) additionalAction = true;
                 }
 
-                //we activate a leader card if possible
-                if (/*action == 4 &&*/ leaderaction < 2 /*||*/ && additionalAction) {
+
+                if (leaderaction < 2  && additionalAction) {
                     client.sendMessage("Do you want to remove a card or to activate one? (remove/activate) ");
                     String choice = client.receiveMessage();
 
@@ -289,7 +226,6 @@ public class TurnManager {
 
                     additionalAction = false;
 
-                    //player.updatePlayerBoard(client, game);
                 }
 
             } catch (IOException e) {
@@ -297,13 +233,10 @@ public class TurnManager {
             }
             c = msg;
         } while(!c.equals("1") && !c.equals("2") && !c.equals("3")|| !actionDone);
-        //Ad ogni turno, effettuo il controllo del Vatican Report e
-        // notifico tutti gli observer dei cambiamenti avvenuti durante il turno
+
 
 
         player.getFaithTrack().callVaticanReport(player, game);
-        //player.updatePlayerBoard(client, game);
-
         MessageGameManager.personalChanges(client, game.getPlayers().get(actualplayer), game);
         MessageGameManager.generalChanges(client);
 
@@ -311,7 +244,3 @@ public class TurnManager {
 }
 
 
-// CONTROLLER:
-// IN) QUALE CARTA VUOI PRODURRE? LEADER/DEVELOPEMENT e SE CONTINUARE A PRODURRE ---> PRINT
-// OUT1)
-// OUT2) DO PRODUCTION: RIMUOVE DA STORAGE E AGGIUNGE IN STRONGBOX
