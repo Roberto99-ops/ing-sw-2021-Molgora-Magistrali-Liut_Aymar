6dc9c929b762d3dbb8f1a932cf9ef5a0480ad5d0
fm <francesco.magistrali@mail.polimi.it>
2021-06-27 11:24:47 +0200

    Class' coding cleaned

--- src/main/java/it/polimi/ingsw/Server/GameHandler.java
+++ src/main/java/it/polimi/ingsw/Server/GameHandler.java
@@ -6,21 +6,6 @@ import it.polimi.ingsw.model.*;
 import java.io.IOException;
 
 
-/*
-
-rappresenta il client dentro al server
-obeserver observable su model internet, comunica al controller le scelte dell'utente.
-una classe che osserva il model e che se la virtual view riceve le notifiche lo sa
-
-
-client - clienthendler - controller - model - virtualviewvedemodelmodificato -
-
-clienthendler grazie alla virtual view aggiorna l'utente usando pattern state per sveglire il tipo di messaggio
-da mandare (json o stringa o...) se si possono inviare file facciamo file json (oppure istanze di classi messaggio?)
-per aggiornamenti del tipo planciastringhe per messaggio "video" esempio su chi ha vinto
-
-*/
-
 
 public class GameHandler extends Player {
 
@@ -44,12 +29,6 @@ public class GameHandler extends Player {
 
     }
 
-  /*  public void updateDevelDeck(ClientHandler clh) throws IOException {
-        DevelopeDecks playercards = clh.getPlayer().getTopCardsOnBoard();
-        DevelopeCardsMsg msg = new DevelopeCardsMsg(playercards);
-        clh.sendMessage(msg);
-    }*/
-
 
     public void updateLeaderDeck(ClientHandler clh) throws IOException {
         LeaderDeck actualLeaderDeck = Game.getLeaderdeck();
@@ -57,83 +36,10 @@ public class GameHandler extends Player {
         clh.sendMessage(msg);
     }
 
+
     public void updatePlayerBoard(ClientHandler clh, Game game) throws IOException {
         PlayerMsg msg = new PlayerMsg(this, game);
         clh.sendMessage(msg);
     }
-
-
-
-
-
-
-
-
-/*
-    public void updateStorage(ClientHandler clh) throws IOException {
-        Storage actualStorage = this.getStorage();
-        StorageMsg msg = new StorageMsg(actualStorage);
-        clh.sendMessage(msg);
-    }
-
-    public void updateStrongbox(ClientHandler clh) throws IOException {
-        StrongBox actualStrongbox = this.getStrongBox();
-        StrongboxMsg msg = new StrongboxMsg(actualStrongbox.getStructure());
-        clh.sendMessage(msg);
-    }
-
-
-    public void updateDevelopementSpace(ClientHandler clh) throws IOException {
-        DevelopementSpace actualDevelopementSpace = this.getDSpace();
-
-
-    }
-
-    public static void updateTimer_VR (ClientHandler clh) throws IOException {
-        int actualTimer_VR = Game.getTimer_VR();
-        clh.sendMessage("new Timer_VR" + actualTimer_VR);
-
-    }
-
-    public static void updateVR (ClientHandler clh) throws IOException {
-        int actualVR = Game.getVR();
-        clh.sendMessage("new Timer_VR" + actualVR);
-
-    }
-
-   public void updatePV (ClientHandler clh) throws IOException {
-       int actualPV = this.getPv();
-       clh.sendMessage("new PV" + actualPV);
-   }
-
-
-    public void updateLeaderCards (ClientHandler clh) throws IOException {
-        LeaderDeck actualLeaderDeck = this.getLeadercards();
-        LeaderDeckMsg msg = new LeaderDeckMsg(actualLeaderDeck);
-        clh.sendMessage(msg);
-
-    }
-
-
-    public void updateSkill1 (ClientHandler clh) throws IOException {
-        int actualSkiil1 = this.getSkill1();
-        clh.sendMessage("new Skiil1" + actualSkiil1);
-    }
-
-    public void updateSkill2 (ClientHandler clh) throws IOException {
-        int actualSkiil2 = this.getSkill2();
-        clh.sendMessage("new Skiil2" + actualSkiil2);
-    }
-
-    public void updateDevelopementQuantity (ClientHandler clh) throws IOException {
-        int actualDQ = this.getSkill2();
-        clh.sendMessage("new DevelopementQuantity" + actualDQ);
-    }
-
-    public void updateFaithTrack (ClientHandler clh) throws IOException {
-        FaithTrack actualFaithtrack = this.getFaithTrack();
-        clh.sendMessage(actualFaithtrack);
-    }
-*/
 
 }
