ac60ae4beff9bcd0ad89c3072e2f44b660440ecf
fm <francesco.magistrali@mail.polimi.it>
2021-06-28 23:33:01 +0200

    LeaderDeckmsg cleaned from comments

--- src/main/java/it/polimi/ingsw/Server/messages/LeaderDeckMsg.java
+++ src/main/java/it/polimi/ingsw/Server/messages/LeaderDeckMsg.java
@@ -9,53 +9,25 @@ import java.util.ArrayList;
  * The structure sent is an array of integer.
  * Every element of the array is the position fo that card into the json file.
  */
-public class LeaderDeckMsg extends NetworkMessage{
-    private LeaderDeck leaderDeck= new LeaderDeck();
-    //private ArrayList<Integer> array;
+public class LeaderDeckMsg extends NetworkMessage {
+
+    private LeaderDeck leaderDeck = new LeaderDeck();
 
     /**
      * Setter for the instance 'leaderDeck': it will be the data sent to the client
      * @param leaderDeck
      */
+
     public LeaderDeckMsg(LeaderDeck leaderDeck){
         this.leaderDeck=leaderDeck;
     }
-
-    /*public LeaderDeckMsg()
-    {
-        array = new ArrayList<>();
-    }
-     */
-
-
-    /*
-    public ArrayList<Integer> getArray() {
-        return array;
-    }
-
-     */
 
     /**
      * Getter for the data we want the Server to send to the Client
      * @return
      */
-    public LeaderDeck getDeck(){return leaderDeck;}
 
-    /*
-     * This method has to be called by the receiver, to "unpack" the array of integer and
-     * trasform it into a deck of leadercard.
-     * @return the deck of leadercard
-     * @throws FileNotFoundException
+    public LeaderDeck getDeck(){ return leaderDeck;}
 
-    public LeaderDeck getDeck() throws FileNotFoundException {
-        LeaderDeck deck = new LeaderDeck();
-        for (int i = 0; i < this.getArray().size(); i++) {
-            LeaderCard card = new LeaderCard();
-            card.setCard(this.getArray().get(i));
-            deck.getStructure().add(card);
-        }
-        return deck;
-    }
 
-     */
 }
